12a423c519dbbe23f8b01d443dbca18293703678
Tim Sanwald <sanwald.tim@googlemail.com>
2014-07-30 22:29:27 +0200

    Added state chart project and added dependencies

---
+++ CoWolf-Abstract-Model/src/main/java/de/uni_stuttgart/informatik/iste/cowolf/model/AbstractArchitectureModel.java
@@ -0,0 +1,5 @@
+package de.uni_stuttgart.informatik.iste.cowolf.model;
+
+public class AbstractArchitectureModel implements IModel {
+
+}
---
+++ CoWolf-Abstract-Model/src/main/java/de/uni_stuttgart/informatik/iste/cowolf/model/AbstractQoSModel.java
@@ -0,0 +1,5 @@
+package de.uni_stuttgart.informatik.iste.cowolf.model;
+
+public class AbstractQoSModel implements IModel {
+
+}
