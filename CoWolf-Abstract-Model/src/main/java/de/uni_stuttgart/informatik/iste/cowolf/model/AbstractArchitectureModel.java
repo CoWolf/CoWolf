f3a4e0930302ac81c1fc3f0f6791b2df7885fa95
ReneTrefft <trefftre@studi.informatik.uni-stuttgart.de>
2014-07-31 02:07:26 +0200

    completely recreated all CoWolf projects according to Eclipse conventions; abstract projects are now *.model and *.transformation; created separate test projects (suffix .tests) with maven/tycho packaging "eclipse-test-plugin"; fixed some typos

--- CoWolf-Abstract-Model/src/main/java/de/uni_stuttgart/informatik/iste/cowolf/model/AbstractArchitectureModel.java
+++
@@ -1,5 +0,0 @@
-package de.uni_stuttgart.informatik.iste.cowolf.model;
-
-public class AbstractArchitectureModel implements IModel {
-
-}
--- CoWolf-Abstract-Model/src/main/java/de/uni_stuttgart/informatik/iste/cowolf/model/AbstractQoSModel.java
+++
@@ -1,5 +0,0 @@
-package de.uni_stuttgart.informatik.iste.cowolf.model;
-
-public class AbstractQoSModel implements IModel {
-
-}
