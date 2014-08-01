fb0d1ba2755c1a564b202f610dff40524f6d6f67
Tim Sanwald <sanwald.tim@googlemail.com>
2014-08-01 14:45:01 +0200

    Added evolution interface and implementation of it by statechart and dtmc

---
+++ de.uni_stuttgart.iste.cowolf.evolution.statechart/src/de/uni_stuttgart/iste/cowolf/evolution/statechart/StatechartEvolutionManager.java
@@ -0,0 +1,11 @@
+package de.uni_stuttgart.iste.cowolf.evolution.statechart;
+
+import de.uni_stuttgart.iste.cowolf.evolution.AbstractEvolutionManager;
+
+public class StatechartEvolutionManager extends AbstractEvolutionManager {
+
+	public StatechartEvolutionManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
