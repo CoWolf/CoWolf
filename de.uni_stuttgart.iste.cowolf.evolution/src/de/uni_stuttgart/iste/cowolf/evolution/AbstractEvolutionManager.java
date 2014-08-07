7bd5c4a5e9b806f5202941765fae5f24f3d5ff70
Michael Müller <michael.mueller.90@gmail.com>
2014-08-07 13:07:09 +0200

    re-commit AbstractEvolutionManager

--- de.uni_stuttgart.iste.cowolf.evolution/src/de/uni_stuttgart/iste/cowolf/evolution/AbstractEvolutionManager.java
+++ de.uni_stuttgart.iste.cowolf.evolution/src/de/uni_stuttgart/iste/cowolf/evolution/AbstractEvolutionManager.java
@@ -1,18 +1,114 @@
 package de.uni_stuttgart.iste.cowolf.evolution;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.sidiff.common.emf.exceptions.InvalidModelException;
+import org.sidiff.difference.lifting.facade.LiftingFacade;
+import org.sidiff.difference.lifting.settings.LiftingSettings;
+import org.sidiff.difference.lifting.settings.LiftingSettings.RecognitionEngineMode;
+import org.sidiff.difference.matcher.IMatcher;
+import org.sidiff.difference.matcher.namedelement.NamedElementMatcher;
+import org.sidiff.difference.symmetric.SymmetricDifference;
+import org.silift.common.util.access.EMFModelAccessEx;
+import org.silift.common.util.emf.Scope;
+import org.silift.common.util.exceptions.NoCorrespondencesException;
 
-
+/**
+ * Abstract implementation of the manager that handles evolution of a single
+ * model type. Has to be extended by the specific evolution plugin for a model
+ * type.
+ * 
+ * @author Michael Müller
+ *
+ */
 public abstract class AbstractEvolutionManager {
+	public static final String MATCHER = NamedElementMatcher.KEY;
 
 	public abstract boolean isManaged(EPackage model);
 
-	public List<ModelOperation> evolve(EPackage oldModel, EPackage newModel) {
-		//TODO just an interface stub
+	/**
+	 * This method handles the evolution between two models of the same class.
+	 * It returns the differences between two models.
+	 * 
+	 * @param oldModel
+	 *            old model for comparison
+	 * @param newModel
+	 *            new model for comparison
+	 * @return differences between two models.
+	 */
+	public SymmetricDifference evolve(EPackage oldModel, EPackage newModel) {
+		if (!this.isManaged(oldModel) || !this.isManaged(newModel)) {
+			// TODO: return value?
+			return null;
+		}
+		// do required pre-computing work
+		Resource oldResource = oldModel.eResource();
+		Resource newResource = newModel.eResource();
+		String documentType = EMFModelAccessEx
+				.getCharacteristicDocumentType(oldResource);
+		LiftingSettings settings = this.getDefaultSettings(documentType,
+				oldResource, newResource);
+		try {
+			return LiftingFacade.liftMeUp(oldResource, newResource, settings);
+		} catch (NoCorrespondencesException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	public abstract EvolutionTypeInfo getEvolutionTypeInfo();
+
+	/**
+	 * This method returns default settings for generating differences between
+	 * models. Can be overwritten by inheriting managers.
+	 * 
+	 * @param documentType
+	 * @param modelA
+	 * @param modelB
+	 * @return settings object.
+	 */
+	protected LiftingSettings getDefaultSettings(String documentType,
+			Resource modelA, Resource modelB) {
+		LiftingSettings settings = new LiftingSettings(documentType);
+		settings.setRecognitionEngineMode(RecognitionEngineMode.LIFTING_AND_POST_PROCESSING);
+		settings.setMatcher(this.getMatcher(MATCHER, modelA, modelB));
+		settings.setScope(Scope.RESOURCE);
+		return settings;
+	}
+
+	/**
+	 * Get matcher for two models by key.
+	 * 
+	 * @param matcherKey
+	 *            Key to use.
+	 * @param modelA
+	 *            Resource for model A.
+	 * @param modelB
+	 *            Resource for model B.
+	 * @return
+	 */
+	private IMatcher getMatcher(String matcherKey, Resource modelA,
+			Resource modelB) {
+		Set<IMatcher> matchers = LiftingFacade.getAvailableMatchers(modelA,
+				modelB);
+		Map<String, IMatcher> key2matcher = new HashMap<String, IMatcher>();
+		for (IMatcher matcher : matchers) {
+			key2matcher.put(matcher.getKey(), matcher);
+		}
+
+		// Assert matcher is available
+		assert (key2matcher.containsKey(matcherKey)) : "Matcher with key '"
+				+ matcherKey + "' not found!";
+
+		return key2matcher.get(matcherKey);
+	}
+
 }
