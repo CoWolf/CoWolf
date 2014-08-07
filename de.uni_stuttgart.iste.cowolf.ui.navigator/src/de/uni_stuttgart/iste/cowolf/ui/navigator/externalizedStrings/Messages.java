3f68d8d43846739ef696acd91b8b8f770e2a8efc
verenak <verena.kaefer@googlemail.com>
2014-08-07 11:07:19 +0200

    added analyze menu entry

--- de.uni_stuttgart.iste.cowolf.ui.navigator/src/de/uni_stuttgart/iste/cowolf/ui/navigator/externalizedStrings/Messages.java
+++ de.uni_stuttgart.iste.cowolf.ui.navigator/src/de/uni_stuttgart/iste/cowolf/ui/navigator/externalizedStrings/Messages.java
@@ -5,6 +5,7 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "de/uni_stuttgart/iste/cowolf/ui/navigator/externalizedStrings/messages"; //$NON-NLS-1$
 	public static final String CoEvolve_co_evolve_menu = "Co-Evolve";
+	public static final String Analyze_analyze_menu = "Analyze";
 	public static final String Evolve_evolve_menu = "Evolve";
 
 	static {
