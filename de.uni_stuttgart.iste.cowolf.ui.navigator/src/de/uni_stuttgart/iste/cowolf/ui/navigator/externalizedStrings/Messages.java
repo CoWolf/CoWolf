ff1548ae96b89288793ac28c94cfc76c0182b9cb
verenak <verena.kaefer@googlemail.com>
2014-08-07 10:42:08 +0200

    externalized strings

---
+++ de.uni_stuttgart.iste.cowolf.ui.navigator/src/de/uni_stuttgart/iste/cowolf/ui/navigator/externalizedStrings/Messages.java
@@ -0,0 +1,17 @@
+package de.uni_stuttgart.iste.cowolf.ui.navigator.externalizedStrings;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "de/uni_stuttgart/iste/cowolf/ui/navigator/externalizedStrings/messages"; //$NON-NLS-1$
+	public static final String CoEvolve_co_evolve_menu = "Co-Evolve";
+	public static final String Evolve_evolve_menu = "Evolve";
+
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
