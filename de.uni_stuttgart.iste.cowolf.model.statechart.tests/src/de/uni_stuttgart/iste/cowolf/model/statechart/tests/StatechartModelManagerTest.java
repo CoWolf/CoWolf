408b84c5cebcb395a9f7a38ac5bc1a1f4c665128
Jonas Scheurich <jonasscheurich@gmx.net>
2014-08-11 19:50:17 +0200

    Statechart: Test StatechartModelManager

---
+++ de.uni_stuttgart.iste.cowolf.model.statechart.tests/src/de/uni_stuttgart/iste/cowolf/model/statechart/tests/StatechartModelManagerTest.java
@@ -0,0 +1,39 @@
+package de.uni_stuttgart.iste.cowolf.model.statechart.tests;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.junit.Test;
+
+import de.uni_stuttgart.iste.cowolf.model.statechart.StatechartModelManager;
+import de.uni_stuttgart.iste.cowolf.model.statechart.Statechart.StateMachine;
+import de.uni_stuttgart.iste.cowolf.model.statechart.Statechart.StatechartemfFactory;
+
+public class StatechartModelManagerTest {
+
+	@Test
+	public void testIsManaged() {
+		
+		//create test model
+		StatechartemfFactory factory = StatechartemfFactory.eINSTANCE;
+		
+		StateMachine root = factory.createStateMachine();
+		
+		// Create a resource set.
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource res = resourceSet.createResource(URI.createFileURI("myTestStatechart.statechartemf"));
+		
+		res.getContents().add(root);
+		
+		//create test target
+		StatechartModelManager target = new StatechartModelManager();
+		
+		Assert.isTrue(target.isManaged(res));
+		
+	}
+
+}
