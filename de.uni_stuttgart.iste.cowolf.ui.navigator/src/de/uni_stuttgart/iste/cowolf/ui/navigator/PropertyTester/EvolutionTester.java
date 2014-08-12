ebc042019b4aa0b106ba9b0c6d73468761bcd6c1
Michael Müller <michael.mueller.90@gmail.com>
2014-08-12 13:59:47 +0200

    fixed NPE

--- de.uni_stuttgart.iste.cowolf.ui.navigator/src/de/uni_stuttgart/iste/cowolf/ui/navigator/PropertyTester/EvolutionTester.java
+++ de.uni_stuttgart.iste.cowolf.ui.navigator/src/de/uni_stuttgart/iste/cowolf/ui/navigator/PropertyTester/EvolutionTester.java
@@ -26,77 +26,77 @@ import de.uni_stuttgart.iste.cowolf.evolution.AbstractEvolutionManager;
  */
 public class EvolutionTester extends PropertyTester {
 
-	public static final String PROPERTY_NAMESPACE = "de.uni_stuttgart.iste.cowolf.ui.navigator.propertyTester.evolution";
-	public static final String PROPERTY_CAN_FOO = "canFoo";
+    public static final String PROPERTY_NAMESPACE = "de.uni_stuttgart.iste.cowolf.ui.navigator.propertyTester.evolution";
+    public static final String PROPERTY_CAN_FOO = "canFoo";
 
-	private ExtensionHandler extensionHandler;
-	/**
-	 * Constructor
-	 */
-	public EvolutionTester() {
-		this.extensionHandler = new ExtensionHandler();
-	}
+    private ExtensionHandler extensionHandler;
+    /**
+     * Constructor
+     */
+    public EvolutionTester() {
+        this.extensionHandler = new ExtensionHandler();
+    }
 
-	@Override
-	public boolean test(Object receiver, String property, Object[] args,
-			Object expectedValue) {
+    @Override
+    public boolean test(Object receiver, String property, Object[] args,
+            Object expectedValue) {
 
-		// gets the currently selected files
-		IWorkbenchWindow window = PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow();
-		if (window.getSelectionService() == null) {
-			return false;
-		}
-		IStructuredSelection selection = (IStructuredSelection) window
-				.getSelectionService().getSelection();
+        // gets the currently selected files
+        IWorkbenchWindow window = PlatformUI.getWorkbench()
+                .getActiveWorkbenchWindow();
+        if (window == null || window.getSelectionService() == null) {
+            return false;
+        }
+        IStructuredSelection selection = (IStructuredSelection) window
+                .getSelectionService().getSelection();
 
-		if (selection == null) {
-			return false;
-		}
-		List list = selection.toList();
-		if (list.size() == 1) {
-			Object firstElement = list.get(0);
-			if (firstElement instanceof IFile) {
-				return this
-						.getEvolutionManagerForListItem((IFile) firstElement) != null;
-			}
-		}
-		if (list.size() == 2) {
+        if (selection == null) {
+            return false;
+        }
+        List list = selection.toList();
+        if (list.size() == 1) {
+            Object firstElement = list.get(0);
+            if (firstElement instanceof IFile) {
+                return this
+                        .getEvolutionManagerForListItem((IFile) firstElement) != null;
+            }
+        }
+        if (list.size() == 2) {
 
-			Object firstElement = list.get(0);
-			Object secondElement = list.get(1);
+            Object firstElement = list.get(0);
+            Object secondElement = list.get(1);
 
-			if (firstElement instanceof IFile && secondElement instanceof IFile) {
-				// both selected models are of the same type if the returned
-				// evolution managers are equal
-				AbstractEvolutionManager firstElementEvolutionManager = this
-						.getEvolutionManagerForListItem((IFile) firstElement);
-				AbstractEvolutionManager secondElementEvolutionManager = this
-						.getEvolutionManagerForListItem((IFile) secondElement);
-				if (firstElementEvolutionManager != null
-						&& secondElementEvolutionManager != null) {
-					return firstElementEvolutionManager
-							.equals(secondElementEvolutionManager);
-				}
+            if (firstElement instanceof IFile && secondElement instanceof IFile) {
+                // both selected models are of the same type if the returned
+                // evolution managers are equal
+                AbstractEvolutionManager firstElementEvolutionManager = this
+                        .getEvolutionManagerForListItem((IFile) firstElement);
+                AbstractEvolutionManager secondElementEvolutionManager = this
+                        .getEvolutionManagerForListItem((IFile) secondElement);
+                if (firstElementEvolutionManager != null
+                        && secondElementEvolutionManager != null) {
+                    return firstElementEvolutionManager
+                            .equals(secondElementEvolutionManager);
+                }
 
-			}
+            }
 
-		}
-		return false;
-	}
+        }
+        return false;
+    }
 
-	/**
-	 * Returns for a given file the matching EvolutionManager
-	 * 
-	 * @param item
-	 * @return
-	 */
-	private AbstractEvolutionManager getEvolutionManagerForListItem(IFile item) {
-		// transforms the two selected models to Resources
-		URI uri = URI.createPlatformResourceURI(item.getFullPath().toString(),
-				true);
-		ResourceSet resourceSet = new ResourceSetImpl();
-		Resource firstElementResource = resourceSet.getResource(uri, true);
-		return this.extensionHandler.getEvolutionManager(firstElementResource);
-	}
+    /**
+     * Returns for a given file the matching EvolutionManager
+     * 
+     * @param item
+     * @return
+     */
+    private AbstractEvolutionManager getEvolutionManagerForListItem(IFile item) {
+        // transforms the two selected models to Resources
+        URI uri = URI.createPlatformResourceURI(item.getFullPath().toString(),
+                true);
+        ResourceSet resourceSet = new ResourceSetImpl();
+        Resource firstElementResource = resourceSet.getResource(uri, true);
+        return this.extensionHandler.getEvolutionManager(firstElementResource);
+    }
 }
