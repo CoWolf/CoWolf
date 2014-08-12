da5bd2e4a719de10b7d5b143ca48d0971c412ccc
Manuel <alejo.borja@gmail.com>
2014-08-13 00:46:11 +0200

    Safety model

--- de.uni_stuttgart.iste.cowolf.model.fault_tree/src/de/uni_stuttgart/iste/cowolf/model/fault_tree/FaultTreeModelManager.java
+++ de.uni_stuttgart.iste.cowolf.model.fault_tree/src/de/uni_stuttgart/iste/cowolf/model/fault_tree/FaultTreeModelManager.java
@@ -2,18 +2,13 @@ package de.uni_stuttgart.iste.cowolf.model.fault_tree;
 
 import java.util.Map;
 
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
-import org.eclipse.xtext.parser.IEncodingProvider;
-import org.eclipse.xtext.service.AbstractGenericModule;
-
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 
 import de.uni_stuttgart.iste.cowolf.model.AbstractQoSModelManager;
 import de.uni_stuttgart.iste.cowolf.model.IAnalysisListener;
 import de.uni_stuttgart.iste.cowolf.model.ModelTypeInfo;
-import de.uni_stuttgart.iste.cowolf.model.fault_tree.analyze.XFTAGenerator;
+import de.uni_stuttgart.iste.cowolf.model.fault_tree.analyze.FaultTreeAnalyzeJob;
 
 public class FaultTreeModelManager extends AbstractQoSModelManager {
 
@@ -30,22 +25,10 @@ public class FaultTreeModelManager extends AbstractQoSModelManager {
 	@Override
 	public String analyze(Resource model, Map<String, Object> parameters,
 			final IAnalysisListener listener) {
-		// final FaultTreeAnalyzeJob job = new FaultTreeAnalyzeJob(model,
-		// parameters);
-		// job.setPriority(Job.LONG);
-		// job.schedule();
-
-		XFTAGenerator generator = new XFTAGenerator();
-		Injector i = Guice.createInjector(new AbstractGenericModule() {
-			@SuppressWarnings("all")
-			public Class<? extends IEncodingProvider> bindIEncodingProvider() {
-				return IEncodingProvider.Runtime.class;
-			}
-		});
-		JavaIoFileSystemAccess writer = i
-				.getInstance(JavaIoFileSystemAccess.class);
-		writer.setOutputPath("/home/desarrollo/tmp");
-		generator.doGenerate(model, writer);
+		final FaultTreeAnalyzeJob job = new FaultTreeAnalyzeJob(model,
+				parameters);
+		job.setPriority(Job.LONG);
+		job.schedule();
 
 		return null;
 	}
