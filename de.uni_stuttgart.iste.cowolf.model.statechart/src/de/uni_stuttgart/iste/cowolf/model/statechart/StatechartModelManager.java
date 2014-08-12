fd8a2117acd0373524be8f4dc2ea86c0cc3ac245
Michael Müller <michael.mueller.90@gmail.com>
2014-08-12 10:46:11 +0200

    added StatechartModelManager from master #16

---
+++ de.uni_stuttgart.iste.cowolf.model.statechart/src/de/uni_stuttgart/iste/cowolf/model/statechart/StatechartModelManager.java
@@ -0,0 +1,69 @@
+package de.uni_stuttgart.iste.cowolf.model.statechart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+
+import de.uni_stuttgart.iste.cowolf.model.IArchitectureModelManager;
+import de.uni_stuttgart.iste.cowolf.model.ModelTypeInfo;
+import de.uni_stuttgart.iste.cowolf.model.statechart.impl.BooleanExpressionImpl;
+import de.uni_stuttgart.iste.cowolf.model.statechart.impl.CompositeStateImpl;
+import de.uni_stuttgart.iste.cowolf.model.statechart.impl.EventImpl;
+import de.uni_stuttgart.iste.cowolf.model.statechart.impl.GuardImpl;
+import de.uni_stuttgart.iste.cowolf.model.statechart.impl.StateImpl;
+import de.uni_stuttgart.iste.cowolf.model.statechart.impl.StateMachineImpl;
+import de.uni_stuttgart.iste.cowolf.model.statechart.impl.StateVertexImpl;
+import de.uni_stuttgart.iste.cowolf.model.statechart.impl.TransitionImpl;
+
+public class StatechartModelManager implements IArchitectureModelManager {
+
+	@Override
+	public boolean isManaged(Resource model) {
+		
+		if (model == null) {
+			return false;
+		}
+		if (model.getContents() == null || model.getContents().isEmpty()) {
+			return false;
+		}
+		
+		for(EObject theEObject: model.getContents()) {
+			//check if the current eobject is part of the allowed classes list
+			boolean match = false;
+			for (Class theEClass: this.getModelTypeInfo().PROPER_CONTENTS) {
+				if (theEObject.getClass() == theEClass) {
+					match = true;
+				}
+			}
+			//return false if the current theEObject dosn`t match to the items in the theEClass list
+			if (!match) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	@Override
+	public ModelTypeInfo getModelTypeInfo() {
+		
+		List<Class> propercontents = new ArrayList();
+		
+		// create the allowed EClasses
+		propercontents.add(BooleanExpressionImpl.class);
+		propercontents.add(CompositeStateImpl.class);
+		propercontents.add(EventImpl.class);
+		propercontents.add(GuardImpl.class);
+		propercontents.add(StateImpl.class);
+		propercontents.add(StateMachineImpl.class);
+		propercontents.add(StateVertexImpl.class);
+		propercontents.add(TransitionImpl.class);
+		
+		ModelTypeInfo mti = new ModelTypeInfo("statechart", propercontents);
+		
+		return mti;
+	}
+
+}
