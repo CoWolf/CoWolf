1dd35c2d284c246a7c82240421088b1542a019f3
verenak <verena.kaefer@googlemail.com>
2014-08-12 08:50:37 +0200

    javadoc added

---
+++ de.uni_stuttgart.iste.cowolf.ui/src/de/uni_stuttgart/iste/cowolf/ui/wizards/ExportPage.java
@@ -0,0 +1,31 @@
+package de.uni_stuttgart.iste.cowolf.ui.wizards;
+
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.ui.dialogs.WizardExportResourcesPage;
+
+public class ExportPage extends WizardExportResourcesPage {
+
+	protected ExportPage(String pageName, IStructuredSelection selection) {
+		super(pageName, selection);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void createDestinationGroup(Composite parent) {
+		  
+//		  Label label = new Label(parent, SWT.SHADOW_NONE);
+//		  Text text = new Text(parent, SWT.BORDER);
+//		  Button button = new Button(parent, SWT.PUSH);
+
+	}
+	
+}
