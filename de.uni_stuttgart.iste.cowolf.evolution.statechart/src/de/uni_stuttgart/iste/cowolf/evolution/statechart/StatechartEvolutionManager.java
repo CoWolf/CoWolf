725e32ef280d09622ff08411d3ff85d868ae7bc9
Michael Müller <michael.mueller.90@gmail.com>
2014-08-13 11:17:25 +0200

    updated matcher #16

--- de.uni_stuttgart.iste.cowolf.evolution.statechart/src/de/uni_stuttgart/iste/cowolf/evolution/statechart/StatechartEvolutionManager.java
+++ de.uni_stuttgart.iste.cowolf.evolution.statechart/src/de/uni_stuttgart/iste/cowolf/evolution/statechart/StatechartEvolutionManager.java
@@ -6,20 +6,27 @@ import de.uni_stuttgart.iste.cowolf.evolution.AbstractEvolutionManager;
 import de.uni_stuttgart.iste.cowolf.evolution.EvolutionTypeInfo;
 import de.uni_stuttgart.iste.cowolf.model.statechart.StateMachine;
 
+/**
+ * This class handles evolution of a statechart model.
+ * 
+ * @author Michael Müller
+ *
+ */
 public class StatechartEvolutionManager extends AbstractEvolutionManager {
     @Override
-    public boolean isManaged(Resource model) {    	
-        if (model == null || model.getContents() == null || model.getContents().isEmpty()) {
+    public boolean isManaged(Resource model) {
+        if (model == null || model.getContents() == null
+                || model.getContents().isEmpty()) {
             return false;
         }
- 
+
         return model.getContents().get(0) instanceof StateMachine;
     }
 
     @Override
     public EvolutionTypeInfo getEvolutionTypeInfo() {
         EvolutionTypeInfo info = new EvolutionTypeInfo();
-        info.setMatcher(EvolutionTypeInfo.MATCHER_ECOREID);
+        info.setMatcher(EvolutionTypeInfo.MATCHER_EMFCOMPARE);
         return info;
     }
 }
