5b25fea3f39c019487770d4d998adc5a0bced7d8
Tim Sanwald <sanwald.tim@googlemail.com>
2014-08-13 13:58:24 +0200

    latest fixes

--- de.uni_stuttgart.iste.cowolf.model.statechart.tests/src/de/uni_stuttgart/iste/cowolf/model/statechart/tests/StatechartModelManagerTest.java
+++ de.uni_stuttgart.iste.cowolf.model.statechart.tests/src/de/uni_stuttgart/iste/cowolf/model/statechart/tests/StatechartModelManagerTest.java
@@ -1,62 +1,33 @@
 package de.uni_stuttgart.iste.cowolf.model.statechart.tests;
 
-import static org.junit.Assert.*;
-
-import org.eclipse.core.runtime.Assert;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.junit.Test;
-
-import de.uni_stuttgart.iste.cowolf.model.dtmc.DTMCemfFactory;
-import de.uni_stuttgart.iste.cowolf.model.dtmc.Root;
-import de.uni_stuttgart.iste.cowolf.model.statechart.StatechartModelManager;
-import de.uni_stuttgart.iste.cowolf.model.statechart.Statechart.StateMachine;
-import de.uni_stuttgart.iste.cowolf.model.statechart.Statechart.StatechartemfFactory;
 
 public class StatechartModelManagerTest {
+/*
+	Resource res;
+	StateMachine root;
 
-	@Test
-	public void testIsManaged1() {
-		
+	@Before
+	public void machDas() {
 		//create test model
 		StatechartemfFactory factory = StatechartemfFactory.eINSTANCE;
-		
-		StateMachine root = factory.createStateMachine();
-		
+
+		this.root = factory.createStateMachine();
+
 		// Create a resource set.
 		ResourceSet resourceSet = new ResourceSetImpl();
-		Resource res = resourceSet.createResource(URI.createFileURI("myTestStatechart.statechartemf"));
-		
-		res.getContents().add(root);
-		
-		//create test target
-		StatechartModelManager target = new StatechartModelManager();
-		
-		Assert.isTrue(target.isManaged(res));
-		
-	}
-	
-	@Test
-	public void testIsManaged2() {
-		
-		//create test model
-		DTMCemfFactory factory = DTMCemfFactory.eINSTANCE;
-		
-		Root root = factory.createRoot();
-		
-		// Create a resource set.
-		ResourceSet resourceSet = new ResourceSetImpl();
-		Resource res = resourceSet.createResource(URI.createFileURI("myTestStatechart.statechartemf"));
-		
-		res.getContents().add(root);
-		
-		//create test target
-		StatechartModelManager target = new StatechartModelManager();
-		
-		Assert.isTrue(!target.isManaged(res));
-		
+		this.res = resourceSet.createResource(URI.createFileURI("myTestStatechart.statechartemf"));
 	}
 
+	@Test
+	public void testIsManaged1() throws IOException {
+
+		EList<EObject> bla = this.res.getContents();
+		bla.add(this.root);
+
+		//create test target
+		StatechartModelManager target = new StatechartModelManager();
+		Assert.assertTrue(target.isManaged(this.res));
+
+	}
+*/
 }
