a3194477902d670f87ee0a2af08f9b82ef43f86d
verenak <verena.kaefer@googlemail.com>
2014-08-13 14:27:48 +0200

    manager class

--- de.uni_stuttgart.iste.cowolf.model/src/de/uni_stuttgart/iste/cowolf/model/ModelTypeInfo.java
+++ de.uni_stuttgart.iste.cowolf.model/src/de/uni_stuttgart/iste/cowolf/model/ModelTypeInfo.java
@@ -5,17 +5,22 @@ import java.util.List;
 public class ModelTypeInfo {
 
 	/**
-	 * Create a ModelTypeInfo object. This object contains the meta-model-name and the 
-	 * allowed EObject-Implementations. 
-	 * @param metaModelName as string.
-	 * @param propercontents list of .class objects of every allowed EObject implementation.
+	 * Create a ModelTypeInfo object. This object contains the meta-model-name
+	 * and the allowed EObject-Implementations.
+	 * 
+	 * @param metaModelName
+	 *            a string. This will be the name of the automatically created
+	 *            model folder
+	 * @param propercontents
+	 *            list of .class objects of every allowed EObject
+	 *            implementation.
 	 */
 	public ModelTypeInfo(String metaModelName, List<Class<?>> propercontents) {
 		this.METAMODEL_NAME = metaModelName;
 		this.PROPER_CONTENTS = propercontents;
 	}
-	
+
 	public final String METAMODEL_NAME;
-	
+
 	public final List<Class<?>> PROPER_CONTENTS;
 }
