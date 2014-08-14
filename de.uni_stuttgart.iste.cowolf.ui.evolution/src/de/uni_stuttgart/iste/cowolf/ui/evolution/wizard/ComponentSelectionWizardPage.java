f28aed7fe89952294534f7a8b9fd08172584a718
ReneTrefft <trefftre@studi.informatik.uni-stuttgart.de>
2014-08-14 16:34:46 +0200

    removed debugging code; Workspace chooser filter now only shows the supported models of the current project

--- de.uni_stuttgart.iste.cowolf.ui.evolution/src/de/uni_stuttgart/iste/cowolf/ui/evolution/wizard/ComponentSelectionWizardPage.java
+++ de.uni_stuttgart.iste.cowolf.ui.evolution/src/de/uni_stuttgart/iste/cowolf/ui/evolution/wizard/ComponentSelectionWizardPage.java
@@ -161,7 +161,6 @@ public class ComponentSelectionWizardPage extends WizardPage {
 			public void widgetSelected(SelectionEvent e) {
 
 				List<ViewerFilter> filters = new ArrayList<ViewerFilter>();
-				System.out.println("++++"+ wizard.getModelA().getProject().getName());
 				filters.add(new WorkspaceResourceDialogFilter(wizard.getModelA().getProject().getName()));
 				IFile[] files = WorkspaceResourceDialog.openFileSelection(
 						shell, "Choose model file", "Choose model file", true,
