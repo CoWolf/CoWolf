5afb07d14a25a7d266b4ace3713b63d5f3587a36
Michael Müller <michael.mueller.90@gmail.com>
2014-08-14 13:36:46 +0200

    added JAXB objects. #13

--- de.uni_stuttgart.iste.cowolf.transformation/src/de/uni_stuttgart/iste/cowolf/transformation/TransformationTypeInfo.java
+++ de.uni_stuttgart.iste.cowolf.transformation/src/de/uni_stuttgart/iste/cowolf/transformation/TransformationTypeInfo.java
@@ -1,5 +1,18 @@
 package de.uni_stuttgart.iste.cowolf.transformation;
 
-public class TransformationTypeInfo {
+import java.io.File;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import de.uni_stuttgart.iste.cowolf.transformation.mapping.xml.Mapping;
+
+public class TransformationTypeInfo {
+    public static void main(String[] args) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(Mapping.class);
+        Unmarshaller m = context.createUnmarshaller();
+        Mapping map = (Mapping) m.unmarshal(new File("schema/Example.xml"));
+        System.out.println(map.getDifference().size());
+    }
 }
