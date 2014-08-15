2a3b3c23ea7d43676ddfd33c80d90f94ce00e58d
verenak <verena.kaefer@googlemail.com>
2014-08-15 16:25:15 +0200

    new try

---
+++ de.uni_stuttgart.iste.cowolf.model.activity_diagram.tests/src/activity/tests/SplitTest.java
@@ -0,0 +1,70 @@
+/**
+ */
+package activity.tests;
+
+import activity.ActivityFactory;
+import activity.Split;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Split</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class SplitTest extends BarTest {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(SplitTest.class);
+	}
+
+	/**
+	 * Constructs a new Split test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public SplitTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Returns the fixture for this Split test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected Split getFixture() {
+		return (Split)fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(ActivityFactory.eINSTANCE.createSplit());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //SplitTest
