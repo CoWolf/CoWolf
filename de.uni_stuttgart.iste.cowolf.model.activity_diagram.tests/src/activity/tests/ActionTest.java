a90938921fd250152954fa700d80d3ed41d87ff0
verenak <verena.kaefer@googlemail.com>
2014-08-15 16:25:15 +0200

    new try

---
+++ de.uni_stuttgart.iste.cowolf.model.activity_diagram.tests/src/activity/tests/ActionTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package activity.tests;
+
+import activity.Action;
+import activity.ActivityFactory;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Action</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ActionTest extends TestCase {
+
+	/**
+	 * The fixture for this Action test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected Action fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(ActionTest.class);
+	}
+
+	/**
+	 * Constructs a new Action test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ActionTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Action test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(Action fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Action test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected Action getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(ActivityFactory.eINSTANCE.createAction());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //ActionTest
