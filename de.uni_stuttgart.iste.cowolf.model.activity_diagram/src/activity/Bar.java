a646a3cced273986b4256610e60980b200e2f7ca
verenak <verena.kaefer@googlemail.com>
2014-08-15 13:28:54 +0200

    updated eclipse

---
+++ de.uni_stuttgart.iste.cowolf.model.activity_diagram/src/activity/Bar.java
@@ -0,0 +1,18 @@
+/**
+ */
+package activity;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Bar</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see activity.ActivityPackage#getBar()
+ * @model
+ * @generated
+ */
+public interface Bar extends EObject {
+} // Bar
