3a4bd376885dd13192b225a8c26355ae49bfe2ab
verenak <verena.kaefer@googlemail.com>
2014-08-15 13:28:54 +0200

    updated eclipse

---
+++ de.uni_stuttgart.iste.cowolf.model.activity_diagram/src/activity/impl/ActivityFactoryImpl.java
@@ -0,0 +1,194 @@
+/**
+ */
+package activity.impl;
+
+import activity.*;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ActivityFactoryImpl extends EFactoryImpl implements ActivityFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static ActivityFactory init() {
+		try {
+			ActivityFactory theActivityFactory = (ActivityFactory)EPackage.Registry.INSTANCE.getEFactory(ActivityPackage.eNS_URI);
+			if (theActivityFactory != null) {
+				return theActivityFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new ActivityFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ActivityFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case ActivityPackage.ROOT: return createRoot();
+			case ActivityPackage.START: return createStart();
+			case ActivityPackage.ACTION: return createAction();
+			case ActivityPackage.DECISION: return createDecision();
+			case ActivityPackage.BAR: return createBar();
+			case ActivityPackage.SPLIT: return createSplit();
+			case ActivityPackage.JOIN: return createJoin();
+			case ActivityPackage.ARROW: return createArrow();
+			case ActivityPackage.END: return createEnd();
+			case ActivityPackage.CONDITION_ARROW: return createConditionArrow();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Root createRoot() {
+		RootImpl root = new RootImpl();
+		return root;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Start createStart() {
+		StartImpl start = new StartImpl();
+		return start;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Action createAction() {
+		ActionImpl action = new ActionImpl();
+		return action;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Decision createDecision() {
+		DecisionImpl decision = new DecisionImpl();
+		return decision;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Bar createBar() {
+		BarImpl bar = new BarImpl();
+		return bar;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Split createSplit() {
+		SplitImpl split = new SplitImpl();
+		return split;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Join createJoin() {
+		JoinImpl join = new JoinImpl();
+		return join;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Arrow createArrow() {
+		ArrowImpl arrow = new ArrowImpl();
+		return arrow;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public End createEnd() {
+		EndImpl end = new EndImpl();
+		return end;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ConditionArrow createConditionArrow() {
+		ConditionArrowImpl conditionArrow = new ConditionArrowImpl();
+		return conditionArrow;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ActivityPackage getActivityPackage() {
+		return (ActivityPackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static ActivityPackage getPackage() {
+		return ActivityPackage.eINSTANCE;
+	}
+
+} //ActivityFactoryImpl
