cfd4242819a93ee8b195145b0208fd25c3c979d8
Philipp Niethammer <webmaster@nochwer.de>
2014-08-18 09:19:18 +0200

    Rolled back DTMC to b232f25

--- de.uni_stuttgart.iste.cowolf.model.dtmc/src/de/uni_stuttgart/iste/cowolf/model/dtmc/DTMCModelManager.java
+++ de.uni_stuttgart.iste.cowolf.model.dtmc/src/de/uni_stuttgart/iste/cowolf/model/dtmc/DTMCModelManager.java
@@ -39,14 +39,8 @@ public class DTMCModelManager extends AbstractQoSModelManager {
 				|| model.getContents().size() == 0)
 			return false;
 		else
-<<<<<<< HEAD
 			return (model.getContents().get(0) instanceof DTMC);
 
 	}
 	
 }
-=======
-			return (model.getContents().get(0) instanceof Root);
-	}
-}
->>>>>>> 13-safety-certification
