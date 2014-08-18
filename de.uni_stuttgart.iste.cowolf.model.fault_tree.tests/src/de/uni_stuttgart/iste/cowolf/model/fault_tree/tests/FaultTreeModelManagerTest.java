2bbc1f729feae9d20c6b71a0e16fee58b01468fc
verenak <verena.kaefer@googlemail.com>
2014-08-18 15:54:41 +0200

    fixed bug

--- de.uni_stuttgart.iste.cowolf.model.fault_tree.tests/src/de/uni_stuttgart/iste/cowolf/model/fault_tree/tests/FaultTreeModelManagerTest.java
+++ de.uni_stuttgart.iste.cowolf.model.fault_tree.tests/src/de/uni_stuttgart/iste/cowolf/model/fault_tree/tests/FaultTreeModelManagerTest.java
@@ -3,9 +3,7 @@ package de.uni_stuttgart.iste.cowolf.model.fault_tree.tests;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
 import java.io.File;
-
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -14,46 +12,34 @@ import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMLResourceFactoryImpl;
 import org.junit.Before;
 import org.junit.Test;
-
 import de.uni_stuttgart.iste.cowolf.model.fault_tree.FaultTreeModelManager;
 import de.uni_stuttgart.iste.cowolf.model.fault_tree.FaultTreePackage;
 import de.uni_stuttgart.iste.cowolf.model.fault_tree.impl.FaultTreePackageImpl;
 
 public class FaultTreeModelManagerTest {
-
 	private FaultTreeModelManager faultTreeModelManager;
-
 	private Resource model;
 
 	@Before
 	public void setUp() throws Exception {
 		faultTreeModelManager = new FaultTreeModelManager();
-
 		FaultTreePackageImpl.init();
-
 		FaultTreePackage.eINSTANCE.eClass();
-
 		ResourceSet resourceSet = new ResourceSetImpl();
-
 		resourceSet
 				.getResourceFactoryRegistry()
 				.getExtensionToFactoryMap()
 				.put(Resource.Factory.Registry.DEFAULT_EXTENSION,
 						new XMIResourceFactoryImpl());
-
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
 				"xml", new XMLResourceFactoryImpl());
-
 		resourceSet.getPackageRegistry().put(FaultTreePackage.eNS_URI,
 				FaultTreePackage.eINSTANCE);
-
 		String f = "model/scenario_02.faulttree";
 		File file = new File(f);
 		URI uri = file.isFile() ? URI.createFileURI(file.getAbsolutePath())
 				: URI.createURI(f);
-
 		model = resourceSet.getResource(uri, true);
-
 	}
 
 	@Test
@@ -79,5 +65,4 @@ public class FaultTreeModelManagerTest {
 			fail(e.getMessage());
 		}
 	}
-
 }
