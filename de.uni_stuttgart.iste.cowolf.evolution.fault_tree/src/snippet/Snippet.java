e93c4a85659d0e732b203abc7e8f40ef9215fe59
verenak <verena.kaefer@googlemail.com>
2014-08-18 15:41:38 +0200

    Fixed pom dependencies to new plugins

---
+++ de.uni_stuttgart.iste.cowolf.evolution.fault_tree/src/snippet/Snippet.java
@@ -0,0 +1,16 @@
+package snippet;
+
+public class Snippet {
+	 <extension
+	         id="dtmcModelManager"
+	         point="de.uni_stuttgart.iste.cowolf.model.qosModelManagerExtension">
+	      <qosModelManager
+	            class="de.uni_stuttgart.iste.cowolf.model.dtmc.DTMCModelManager">
+	      </qosModelManager>
+	   </extension>
+	
+	   <extension point="org.eclipse.emf.ecore.generated_package">
+	      <!-- @generated DTMCemf -->
+	      <
+}
+
