16b1eeccb7a2cc4ed0607b4ceba9205d7c69df16
zimmerml <zimmerml@users.noreply.github.com>
2014-08-22 14:51:55 +0200

    ref #148
    old edit-, recognitionrules and rulebase deleted. New ones created and
    added

--- de.uni_stuttgart.iste.cowolf.evolution.dtmc.recognitionrules/src/de/uni_stuttgart/iste/cowolf/evolution/dtmc/recognitionrules/DTMCAtomicGeneratedRulebase.java
+++ de.uni_stuttgart.iste.cowolf.evolution.dtmc.recognitionrules/src/de/uni_stuttgart/iste/cowolf/evolution/dtmc/recognitionrules/DTMCAtomicGeneratedRulebase.java
@@ -6,14 +6,15 @@ public class DTMCAtomicGeneratedRulebase extends AbstractRuleBase {
 
 	public static final String BUNDLE_ID = "de.uni_stuttgart.iste.cowolf.evolution.dtmc.recognitionrules";
 	public static final String RULE_BASE_DIR = "rulebase";
-	public static final String RULE_BASE_NAME = "dtmc";
+	public static final String RULE_BASE_NAME = "DTMCGenerated";
 	public static final String RULE_BASE_EXT = ".rulebase";
-	
+
 	public static final String EDIT_RULES_PLUGIN = "de.uni_stuttgart.iste.cowolf.evolution.dtmc.editrules";
-	
+
 	@Override
 	protected String getRuleBaseURI() {
-		return "/" + BUNDLE_ID + "/" + RULE_BASE_DIR + "/" + RULE_BASE_NAME + RULE_BASE_EXT;
+		return "/" + BUNDLE_ID + "/" + RULE_BASE_DIR + "/" + RULE_BASE_NAME
+				+ RULE_BASE_EXT;
 	}
 
 	@Override
