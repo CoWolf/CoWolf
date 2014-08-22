0643f83c413e1ed798dc7bb92ace09c03b7a4de7
Tim Sanwald <sanwald.tim@googlemail.com>
2014-08-22 17:29:45 +0200

    Added properties

--- de.uni_stuttgart.iste.cowolf.ui.model.dtmc/src/de/uni_stuttgart/iste/cowolf/ui/model/dtmc/export/DtmcPrismExportPage1.java
+++ de.uni_stuttgart.iste.cowolf.ui.model.dtmc/src/de/uni_stuttgart/iste/cowolf/ui/model/dtmc/export/DtmcPrismExportPage1.java
@@ -2,13 +2,13 @@ package de.uni_stuttgart.iste.cowolf.ui.model.dtmc.export;
 
 
 import java.io.File;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Map;
 
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.ICheckStateListener;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
@@ -20,17 +20,22 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.dialogs.ContainerCheckedTreeViewer;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Tree;
 
 public class DtmcPrismExportPage1 extends WizardPage {
 
 	private Composite container;
 	private Text txtPath;
 	private IStructuredSelection selection;
+	private Button btnExportPctlFile;
+	private Button btnProjectstructure;
+	private ContainerCheckedTreeViewer tree;
+	private Button btnOverwriteExistingFiles;
 
 	protected DtmcPrismExportPage1(String pageName, IWorkbench workbench, IStructuredSelection selection) {
 		super(pageName);
@@ -39,15 +44,16 @@ public class DtmcPrismExportPage1 extends WizardPage {
 		this.selection = selection;
 	}
 
+	
 	@Override
-	public void createControl(Composite parent) {
+	public void createControl(final Composite parent) {
 		
 		this.container = new Composite(parent, SWT.NONE);
 		this.setControl(this.container);
 		this.container.setLayout(new GridLayout(1, false));
 		
 		Group grpModelChooser = new Group(container, SWT.NONE);
-		grpModelChooser.setText("Choose model for export:");
+		grpModelChooser.setText("Choose models for export:");
 		grpModelChooser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		GridLayout gl_grpModelChooser = new GridLayout(1, false);
 		gl_grpModelChooser.horizontalSpacing = 10;
@@ -55,7 +61,7 @@ public class DtmcPrismExportPage1 extends WizardPage {
 		gl_grpModelChooser.marginWidth = 10;
 		gl_grpModelChooser.marginHeight = 10;
 		grpModelChooser.setLayout(gl_grpModelChooser);
-		ContainerCheckedTreeViewer tree = new ContainerCheckedTreeViewer(grpModelChooser);
+		tree = new ContainerCheckedTreeViewer(grpModelChooser);
 		Tree tree_1 = tree.getTree();
 		tree_1.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		tree.setContentProvider(new FileTreeContentProvider("dtmcemf", this.selection));
@@ -83,20 +89,17 @@ public class DtmcPrismExportPage1 extends WizardPage {
 		btnBrowse.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		btnBrowse.setText("Browse...");
 		
-		Button btnCleardirectory = new Button(grpOutputPath, SWT.CHECK);
-		btnCleardirectory.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
-		btnCleardirectory.setText("Clear directory before exporting");
-		
-		Button btnProjectstructure = new Button(grpOutputPath, SWT.CHECK);
+		btnProjectstructure = new Button(grpOutputPath, SWT.CHECK);
 		btnProjectstructure.setSelection(true);
 		btnProjectstructure.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
 		btnProjectstructure.setText("Use folder structure");
 		
-		Button btnOverwriteExistingFiles = new Button(grpOutputPath, SWT.CHECK);
+		btnOverwriteExistingFiles = new Button(grpOutputPath, SWT.CHECK);
 		btnOverwriteExistingFiles.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
-		btnOverwriteExistingFiles.setText("Overwrite existing files");
+		btnOverwriteExistingFiles.setText("Overwrite existing file(s) without warning");
 		
-		Button btnExportPctlFile = new Button(grpOutputPath, SWT.CHECK);
+		btnExportPctlFile = new Button(grpOutputPath, SWT.CHECK);
+		btnExportPctlFile.setEnabled(false);
 		btnExportPctlFile.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
 		btnExportPctlFile.setText("Export pctl file");
 		
@@ -109,6 +112,13 @@ public class DtmcPrismExportPage1 extends WizardPage {
 				setPageComplete();
 			}
 		});
+		tree.addSelectionChangedListener(new ISelectionChangedListener() {
+			
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				setPageComplete();
+			}
+		});
 		
 		txtPath.addModifyListener(new ModifyListener() {
 			@Override
@@ -116,27 +126,38 @@ public class DtmcPrismExportPage1 extends WizardPage {
 				setPageComplete();
 			}
 		});
-		SelectionListener listener = new SelectionListener() {
+		
+		btnBrowse.addSelectionListener(new SelectionListener() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				DirectoryDialog browser = new DirectoryDialog(parent.getShell());
+				txtPath.setText(browser.open());
 				setPageComplete();
 			}
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
-				setPageComplete();
+			}
+		});
+		SelectionListener listener = new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
 			}
 		};
-		btnCleardirectory.addSelectionListener(listener);
 		btnExportPctlFile.addSelectionListener(listener);
 		btnOverwriteExistingFiles.addSelectionListener(listener);
 		btnProjectstructure.addSelectionListener(listener);
+		this.setPageComplete();
 	}
-	
+
 	public void setPageComplete() {
 		String path = this.txtPath.getText();
 		File file = new File(path);
+		
 		if (txtPath.getText().isEmpty()) {
-			this.setErrorMessage("Please specify a output path!");
+			this.setErrorMessage("Please specify an output path!");
 			this.setPageComplete(false);
 			return;
 		}
@@ -159,5 +180,12 @@ public class DtmcPrismExportPage1 extends WizardPage {
 
 		this.setErrorMessage(null);
 		this.setPageComplete(true);
+	}
+
+	public void setProperties(Map<String, Object> properties) {
+		properties.put("output_path", txtPath.getText());
+		properties.put("selected_files", tree.getSelection());
+		properties.put("overwrite_permission", this.btnOverwriteExistingFiles.getSelection());
+		properties.put("use_project_structure", this.btnProjectstructure);
 	}
 }
