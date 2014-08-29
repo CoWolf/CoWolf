262c7e65db595313fd2db2346c66190d8f2ac1a3
DavidSteinhart <David.steinhart@t-online.de>
2014-08-29 17:14:22 +0200

    Created Test Editor Wizard

--- de.uni_stuttgart.iste.cowolf.ui.model.ctmc/src/de/uni_stuttgart/iste/cowolf/ui/model/ctmc/analyze/AnalyzeWizardPage2.java
+++ de.uni_stuttgart.iste.cowolf.ui.model.ctmc/src/de/uni_stuttgart/iste/cowolf/ui/model/ctmc/analyze/AnalyzeWizardPage2.java
@@ -5,24 +5,27 @@ import java.util.HashMap;
 import java.util.TreeSet;
 
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.jface.viewers.CheckboxTreeViewer;
+import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.IBaseLabelProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ILabelProviderListener;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TreeNode;
-import org.eclipse.jface.viewers.TreeNodeContentProvider;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.ui.dialogs.ContainerCheckedTreeViewer;
+import org.eclipse.swt.widgets.ScrollBar;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 
 import de.uni_stuttgart.iste.cowolf.model.ctmc.CTMC;
 import de.uni_stuttgart.iste.cowolf.model.ctmc.State;
@@ -32,10 +35,6 @@ public class AnalyzeWizardPage2 extends WizardPage {
 	private Composite container;
 
 	private Resource resource;
-
-	private CheckboxTreeViewer chkList;
-
-	private Button btnAbsorbing;
 
 	private TreeNode labelNode;
 
@@ -52,35 +51,88 @@ public class AnalyzeWizardPage2 extends WizardPage {
 	public void createControl(final Composite parent) {
 		this.container = new Composite(parent, SWT.NONE);
 		this.setControl(this.container);
-		this.container.setLayout(new GridLayout(1, false));
+		this.container.setLayout(new GridLayout(3, false));
 
-		this.chkList = new ContainerCheckedTreeViewer(this.container, SWT.NONE);
-		this.chkList.getTree().setLayoutData(new GridData(GridData.FILL_BOTH));
-		this.chkList.setContentProvider(new TreeNodeContentProvider());
-		this.chkList.setLabelProvider(getLabelProvider());
+		TableViewer tableViewer = new TableViewer(container, SWT.CHECK | SWT.FULL_SELECTION);
+		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+		final Table table = tableViewer.getTable();
+		GridData gd_t = new GridData(SWT.FILL, SWT.FILL, true, true, 3, 1);
+		gd_t.widthHint = 554;
+		table.setLayoutData(gd_t);
+		table.setLinesVisible(true);
 
-		this.chkList.setInput(this.getTree());
-		this.chkList.expandAll();
+		final TableColumn column1 = new TableColumn(table, SWT.CENTER);
+		column1.setResizable(false);
+		column1.setText(" ");
+		column1.setWidth(40);
+		final TableColumn column2 = new TableColumn(table, SWT.LEFT);
+		column2.setText("Name");
+		column2.setWidth(180);
+		final TableColumn column3 = new TableColumn(table, SWT.LEFT);
+		column3.setResizable(false);
+		column3.setText("Property");
+		column3.setWidth(180);
+		table.setHeaderVisible(true);
 
-		this.chkList
-				.addPostSelectionChangedListener(new ISelectionChangedListener() {
-
-					@Override
-					public void selectionChanged(SelectionChangedEvent event) {
-						AnalyzeWizardPage2.this.getContainer().updateButtons();
-					}
-				});
-
-		this.btnAbsorbing = new Button(this.container, SWT.CHECK);
-		this.btnAbsorbing.setSelection(true);
-		this.btnAbsorbing.setText("Include absorbing states");
-		this.btnAbsorbing.addListener(SWT.Selection, new Listener() {
-
+		container.addControlListener(new ControlAdapter() {
 			@Override
-			public void handleEvent(Event event) {
-				AnalyzeWizardPage2.this.getContainer().updateButtons();
+			public void controlResized(ControlEvent e) {
+				// TODO Auto-generated method stub
+				super.controlResized(e);
+				Rectangle area = container.getClientArea();
+				Point size = table.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+
+				ScrollBar vBar = table.getVerticalBar();
+				int width = area.width - table.computeTrim(0, 0, 0, 0).width
+						- vBar.getSize().x;
+				if (size.y > area.height + table.getHeaderHeight()) {
+					// Subtract the scrollbar width from the total column width
+					// if a vertical scrollbar will be required
+					Point vBarSize = vBar.getSize();
+					width -= vBarSize.x;
+				}
+				Point oldSize = table.getSize();
+				if (oldSize.x > area.width) {
+					// table is getting smaller so make the columns
+					// smaller first and then resize the table to
+					// match the client area width
+					column3.setWidth(width - column1.getWidth()
+							- column2.getWidth());
+					
+					table.setSize(area.width, area.height);
+				} else {
+					// table is getting bigger so make the table
+					// bigger first and then make the columns wider
+					// to match the client area width
+					table.setSize(area.width, area.height);
+					column3.setWidth(width - column1.getWidth()
+							- column2.getWidth());
+				}
+
 			}
 		});
+
+		TableItem item1 = new TableItem(table, SWT.NONE);
+		item1.setText(new String[] { null, "Tim" });
+		TableItem item2 = new TableItem(table, SWT.NONE);
+		item2.setText(new String[] { "", "Dave" });
+		TableItem item3 = new TableItem(table, SWT.NONE);
+		item3.setText(new String[] { "", "Johannes" });
+
+		Button btnAdd = new Button(container, SWT.NONE);
+		btnAdd.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false,
+				1, 1));
+		btnAdd.setText("Add");
+
+		Button btnEdit = new Button(container, SWT.NONE);
+		btnEdit.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false,
+				1, 1));
+		btnEdit.setText("Edit");
+
+		Button btnDelete = new Button(container, SWT.NONE);
+		btnDelete.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false,
+				false, 1, 1));
+		btnDelete.setText("Delete");
 	}
 
 	private TreeNode[] getTree() {
@@ -187,23 +239,23 @@ public class AnalyzeWizardPage2 extends WizardPage {
 		TreeSet<String> labels = new TreeSet<String>();
 		TreeSet<String> states = new TreeSet<String>();
 
-		for (Object obj : this.chkList.getCheckedElements()) {
-			if (!(obj instanceof TreeNode)) {
-				continue;
-			}
-			TreeNode node = (TreeNode) obj;
-			if (node.getParent() == null) {
-				continue;
-			}
+		// for (Object obj : this.chkList.getCheckedElements()) {
+		// if (!(obj instanceof TreeNode)) {
+		// continue;
+		// }
+		// TreeNode node = (TreeNode) obj;
+		// if (node.getParent() == null) {
+		// continue;
+		// }
+		//
+		// if (node.getParent().equals(this.labelNode)) {
+		// labels.add((String) node.getValue());
+		// } else if (node.getParent().equals(this.stateNode)) {
+		// states.add(((State) node.getValue()).getId());
+		// }
+		// }
 
-			if (node.getParent().equals(this.labelNode)) {
-				labels.add((String) node.getValue());
-			} else if (node.getParent().equals(this.stateNode)) {
-				states.add(((State) node.getValue()).getId());
-			}
-		}
-
-		properties.put("analyzeAbsorbing", this.btnAbsorbing.getSelection());
+		// properties.put("analyzeAbsorbing", this.btnAbsorbing.getSelection());
 		properties.put("analyzeLabels", labels);
 		properties.put("analyzeStates", states);
 	}
@@ -211,36 +263,38 @@ public class AnalyzeWizardPage2 extends WizardPage {
 	@Override
 	public boolean isPageComplete() {
 		this.setErrorMessage(null);
-		if (chkList.getCheckedElements().length > 2) {
-			return true;
-		} else {
-			// If less than three elements are selected, make sure that at least one proper state is selected (not a top level node).
-			for (Object obj : this.chkList.getCheckedElements()) {
-				if (!(obj instanceof TreeNode)) {
-					continue;
-				}
-				TreeNode node = (TreeNode) obj;
-				if (node.getParent() != null) {
-					return true;
-				}
-			}
-		}
+		// if (chkList.getCheckedElements().length > 2) {
+		// return true;
+		// } else {
+		// // If less than three elements are selected, make sure that at least
+		// // one proper state is selected (not a top level node).
+		// for (Object obj : this.chkList.getCheckedElements()) {
+		// if (!(obj instanceof TreeNode)) {
+		// continue;
+		// }
+		// TreeNode node = (TreeNode) obj;
+		// if (node.getParent() != null) {
+		// return true;
+		// }
+		// }
+		// }
 
-		// If absorbing states are selected and at least one absorbing state exists, evaluation can be performed.
-		if (btnAbsorbing.getSelection()) {
-			if (resource != null
-					&& resource.getContents() != null
-					&& resource.getContents().get(0) != null
-					&& ((CTMC) resource.getContents().get(0)).getStates() != null) {
-				for (State state : ((CTMC) resource.getContents().get(0))
-						.getStates()) {
-					if (state.getOutgoing() == null
-							|| state.getOutgoing().size() == 0) {
-						return true;
-					}
-				}
-			}
-		}
+		// If absorbing states are selected and at least one absorbing state
+		// exists, evaluation can be performed.
+		// if (btnAbsorbing.getSelection()) {
+		// if (resource != null
+		// && resource.getContents() != null
+		// && resource.getContents().get(0) != null
+		// && ((CTMC) resource.getContents().get(0)).getStates() != null) {
+		// for (State state : ((CTMC) resource.getContents().get(0))
+		// .getStates()) {
+		// if (state.getOutgoing() == null
+		// || state.getOutgoing().size() == 0) {
+		// return true;
+		// }
+		// }
+		// }
+		// }
 
 		this.setErrorMessage("Please make at least one selection.");
 		return false;
