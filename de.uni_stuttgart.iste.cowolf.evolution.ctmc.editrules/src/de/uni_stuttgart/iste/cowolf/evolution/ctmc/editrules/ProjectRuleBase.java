0fdb81d6176b6193a2135e6b48b2b2c4a7a056ce
ReneTrefft <trefftre@studi.informatik.uni-stuttgart.de>
2014-09-01 11:14:37 +0200

    ctmc editrules

---
+++ de.uni_stuttgart.iste.cowolf.evolution.ctmc.editrules/src/de/uni_stuttgart/iste/cowolf/evolution/ctmc/editrules/ProjectRuleBase.java
@@ -0,0 +1,15 @@
+package de.uni_stuttgart.iste.cowolf.evolution.ctmc.editrules;
+
+import org.sidiff.difference.rulebase.extension.AbstractProjectRuleBase;
+
+/**
+ * This generated class does not add any functionality to the @link{AbstractProjectRuleBase}
+ * and conforms to the structure needed in such a @link{RuleBaseProjectNature}.
+ * @generated
+ * @see AbstractProjectRuleBase
+ * @see IRuleBase
+ *
+ */
+public class ProjectRuleBase extends AbstractProjectRuleBase {
+
+}
