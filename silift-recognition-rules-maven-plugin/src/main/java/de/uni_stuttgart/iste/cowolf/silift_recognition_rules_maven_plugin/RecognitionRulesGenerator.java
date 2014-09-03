b38921b621ef09bff8b9ed760276b9202bf0a8be
Rene Trefft <trefftre@studi.informatik.uni-stuttgart.de>
2014-09-04 00:57:46 +0200

    some improvements

--- silift-recognition-rules-maven-plugin/src/main/java/de/uni_stuttgart/iste/cowolf/silift_recognition_rules_maven_plugin/RecognitionRulesGenerator.java
+++ silift-recognition-rules-maven-plugin/src/main/java/de/uni_stuttgart/iste/cowolf/silift_recognition_rules_maven_plugin/RecognitionRulesGenerator.java
@@ -11,20 +11,19 @@ import java.util.List;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.sidiff.difference.rulebase.extension.AbstractProjectRuleBase;
 
 /**
- * 
  * Generates the recognition rules that are needed for the evolution of two
  * model versions.
  * 
  * @author Rene Trefft
- *
  */
 public class RecognitionRulesGenerator extends AbstractMojo {
 
 	/**
-	 * 
 	 * Generates the recognition rules from the edit rules in all available
 	 * SiLift rulebase projects.
 	 * 
@@ -53,11 +52,27 @@ public class RecognitionRulesGenerator extends AbstractMojo {
 
 	}
 
+	/**
+	 * The project itself. This parameter is set by Maven.
+	 */
+	@Parameter(property = "project", readonly = true, required = true)
+	private MavenProject project;
+
+	private boolean isRulebaseProject(File projectRoot) {
+		
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		
+	}
+	
+	
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 
-		List<String> rulebaseProjectNames = new ArrayList<String>();
-
+		File projectRoot = project.getBasedir();
+		
+		
+		
+		
 		File projectRoot = new File("").getAbsoluteFile();
 		File workspaceRoot = projectRoot.getParentFile();
 
@@ -67,7 +82,8 @@ public class RecognitionRulesGenerator extends AbstractMojo {
 
 			if (workspaceProject.isDirectory()) {
 
-				File ruleBaseFile = new File(workspaceProject,AbstractProjectRuleBase.RULEBASE_FILE);
+				File ruleBaseFile = new File(workspaceProject,
+						AbstractProjectRuleBase.RULEBASE_FILE);
 
 				if (ruleBaseFile.isFile()) {
 					getLog().debug("Rulebase file found: " + ruleBaseFile);
@@ -77,52 +93,54 @@ public class RecognitionRulesGenerator extends AbstractMojo {
 			}
 
 		}
-		
-		
 
 		for (File ruleBaseProject : ruleBaseProjects) {
 
 			getLog().info("Rulebase project: " + ruleBaseProject.getName());
-			
-			File editRulesDir = new File(ruleBaseProject, AbstractProjectRuleBase.SOURCE_FOLDER);
+
+			File editRulesDir = new File(ruleBaseProject,
+					AbstractProjectRuleBase.SOURCE_FOLDER);
 
 			if (editRulesDir.isDirectory()) {
 
 				List<File> editRules = new ArrayList<File>();
 				listHenshinFilesRecursively(editRulesDir, editRules);
-				
-				
+
 			} else {
 
 				getLog().warn(
-						"Rulebase project \""
-								+ ruleBaseProject.getName() + "\" has no directory \"" + AbstractProjectRuleBase.SOURCE_FOLDER + "\".");
+						"Rulebase project \"" + ruleBaseProject.getName()
+								+ "\" has no directory \""
+								+ AbstractProjectRuleBase.SOURCE_FOLDER + "\".");
 
 			}
 
 		}
 	}
-	
-	private void listHenshinFilesRecursively(File dir, List<File> henshinFiles){
 
-        File[] files = dir.listFiles(new FilenameFilter() {
+	/**
+	 * @param dir
+	 * @param henshinFiles
+	 */
+	private void listHenshinFilesRecursively(File dir, List<File> henshinFiles) {
+
+		File[] files = dir.listFiles(new FilenameFilter() {
+
 			@Override
 			public boolean accept(File dir, String name) {
 				return name.toLowerCase().endsWith(".henshin");
 			}
 		});
 
-        for(File file: files){
-        	
-            if(file.isDirectory())
-            	listHenshinFilesRecursively(file, henshinFiles);
-            else{
-                henshinFiles.add(file);
-            }
-        }
-        
-    }
-	
-	
+		for (File file : files) {
+
+			if (file.isDirectory())
+				listHenshinFilesRecursively(file, henshinFiles);
+			else {
+				henshinFiles.add(file);
+			}
+		}
+
+	}
 
 }
