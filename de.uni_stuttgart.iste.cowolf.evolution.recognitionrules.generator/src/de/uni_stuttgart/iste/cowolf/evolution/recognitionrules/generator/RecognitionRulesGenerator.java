59c7b5b14e164d8978864e0052126802f8211cbe
ReneTrefft <trefftre@studi.informatik.uni-stuttgart.de>
2014-09-03 21:53:20 +0200

    current state of silift rec. rules generator maven plugin

--- de.uni_stuttgart.iste.cowolf.evolution.recognitionrules.generator/src/de/uni_stuttgart/iste/cowolf/evolution/recognitionrules/generator/RecognitionRulesGenerator.java
+++ de.uni_stuttgart.iste.cowolf.evolution.recognitionrules.generator/src/de/uni_stuttgart/iste/cowolf/evolution/recognitionrules/generator/RecognitionRulesGenerator.java
@@ -1,10 +1,20 @@
 package de.uni_stuttgart.iste.cowolf.evolution.recognitionrules.generator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+import java.net.URL;
 
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
 import org.sidiff.difference.rulebase.builder.RuleBaseBuilder;
 
 /**
@@ -19,31 +29,59 @@ public class RecognitionRulesGenerator {
 
 	/**
 	 * 
-	 * Generates the recognition rules from the edit rules in all available SiLift
-	 * rulebase projects.
+	 * Generates the recognition rules from the edit rules in all available
+	 * SiLift rulebase projects.
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-RuleBaseBuilder ruleBaseBuilder = new RuleBaseBuilder();
-
-		// Uses reflection to invoke private build method in SiLift
-		// RuleBaseBuilder bundle
+		
+		
+		Bundle bundle = Platform.getBundle("de.uni_stuttgart.iste.cowolf.evolution.recognitionrules.generator");
+		URL fileURL = bundle.getEntry("files/test.txt");
+		File file = null;
 		try {
-			Method recognitionRulesFullBuildMethod = RuleBaseBuilder.class
-					.getDeclaredMethod("fullBuild", IProgressMonitor.class);
-			
-			
-			
-			recognitionRulesFullBuildMethod.setAccessible(true);
-			recognitionRulesFullBuildMethod.invoke(ruleBaseBuilder,
-					new NullProgressMonitor());
-		} catch (NoSuchMethodException | SecurityException
-				| IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException e) {
-			e.printStackTrace();
+		    file = new File(FileLocator.resolve(fileURL).toURI());
+		} catch (URISyntaxException e1) {
+		    e1.printStackTrace();
+		} catch (IOException e1) {
+		    e1.printStackTrace();
 		}
 		
+//		URL url;
+//		try {
+//		        url = new URL("platform:/plugin/de.uni_stuttgart.iste.cowolf.evolution.recognitionrules.generator/files/test.txt");
+//		    InputStream inputStream = url.openConnection().getInputStream();
+//		    BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+//		    String inputLine;
+//		 
+//		    while ((inputLine = in.readLine()) != null) {
+//		        System.out.println(inputLine);
+//		    }
+//		 
+//		    in.close();
+//		 
+//		} catch (IOException e) {
+//		    e.printStackTrace();
+//		}
+		
+		
+		// RuleBaseBuilder ruleBaseBuilder = new RuleBaseBuilder();
+		//
+		// // Uses reflection to invoke private build method in SiLift
+		// // RuleBaseBuilder bundle
+		// try {
+		// Method recognitionRulesFullBuildMethod = RuleBaseBuilder.class
+		// .getDeclaredMethod("fullBuild", IProgressMonitor.class);
+		//
+		// recognitionRulesFullBuildMethod.setAccessible(true);
+		// recognitionRulesFullBuildMethod.invoke(ruleBaseBuilder,
+		// new NullProgressMonitor());
+		// } catch (NoSuchMethodException | SecurityException
+		// | IllegalAccessException | IllegalArgumentException
+		// | InvocationTargetException e) {
+		// e.printStackTrace();
+		// }
 
 	}
 
--- silift-recognition-rules-maven-plugin/src/main/java/de/uni_stuttgart/iste/cowolf/silift_recognition_rules_maven_plugin/RecognitionRulesGenerator.java
+++ silift-recognition-rules-maven-plugin/src/main/java/de/uni_stuttgart/iste/cowolf/silift_recognition_rules_maven_plugin/RecognitionRulesGenerator.java
@@ -1,11 +1,17 @@
 package de.uni_stuttgart.iste.cowolf.silift_recognition_rules_maven_plugin;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.sidiff.difference.rulebase.builder.RuleBaseBuilder;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.sidiff.difference.rulebase.extension.AbstractProjectRuleBase;
 
 /**
  * 
@@ -15,36 +21,108 @@ import org.sidiff.difference.rulebase.builder.RuleBaseBuilder;
  * @author Rene Trefft
  *
  */
-public class RecognitionRulesGenerator {
+public class RecognitionRulesGenerator extends AbstractMojo {
 
 	/**
 	 * 
-	 * Generates the recognition rules from the edit rules in all available SiLift
-	 * rulebase projects.
+	 * Generates the recognition rules from the edit rules in all available
+	 * SiLift rulebase projects.
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-RuleBaseBuilder ruleBaseBuilder = new RuleBaseBuilder();
 
-		// Uses reflection to invoke private build method in SiLift
-		// RuleBaseBuilder bundle
-		try {
-			Method recognitionRulesFullBuildMethod = RuleBaseBuilder.class
-					.getDeclaredMethod("fullBuild", IProgressMonitor.class);
-			
-			
-			
-			recognitionRulesFullBuildMethod.setAccessible(true);
-			recognitionRulesFullBuildMethod.invoke(ruleBaseBuilder,
-					new NullProgressMonitor());
-		} catch (NoSuchMethodException | SecurityException
-				| IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		
+		// RuleBaseBuilder ruleBaseBuilder = new RuleBaseBuilder();
+		//
+		// // Uses reflection to invoke private build method in SiLift
+		// // RuleBaseBuilder bundle
+		// try {
+		// Method recognitionRulesFullBuildMethod = RuleBaseBuilder.class
+		// .getDeclaredMethod("fullBuild", IProgressMonitor.class);
+		//
+		//
+		//
+		// recognitionRulesFullBuildMethod.setAccessible(true);
+		// recognitionRulesFullBuildMethod.invoke(ruleBaseBuilder,
+		// new NullProgressMonitor());
+		// } catch (NoSuchMethodException | SecurityException
+		// | IllegalAccessException | IllegalArgumentException
+		// | InvocationTargetException e) {
+		// e.printStackTrace();
+		// }
 
 	}
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+
+		List<String> rulebaseProjectNames = new ArrayList<String>();
+
+		File projectRoot = new File("").getAbsoluteFile();
+		File workspaceRoot = projectRoot.getParentFile();
+
+		List<File> ruleBaseProjects = new ArrayList<File>();
+
+		for (File workspaceProject : workspaceRoot.listFiles()) {
+
+			if (workspaceProject.isDirectory()) {
+
+				File ruleBaseFile = new File(workspaceProject,AbstractProjectRuleBase.RULEBASE_FILE);
+
+				if (ruleBaseFile.isFile()) {
+					getLog().debug("Rulebase file found: " + ruleBaseFile);
+					ruleBaseProjects.add(workspaceProject);
+				}
+
+			}
+
+		}
+		
+		
+
+		for (File ruleBaseProject : ruleBaseProjects) {
+
+			getLog().info("Rulebase project: " + ruleBaseProject.getName());
+			
+			File editRulesDir = new File(ruleBaseProject, AbstractProjectRuleBase.SOURCE_FOLDER);
+
+			if (editRulesDir.isDirectory()) {
+
+				List<File> editRules = new ArrayList<File>();
+				listHenshinFilesRecursively(editRulesDir, editRules);
+				
+				
+			} else {
+
+				getLog().warn(
+						"Rulebase project \""
+								+ ruleBaseProject.getName() + "\" has no directory \"" + AbstractProjectRuleBase.SOURCE_FOLDER + "\".");
+
+			}
+
+		}
+	}
+	
+	private void listHenshinFilesRecursively(File dir, List<File> henshinFiles){
+
+        File[] files = dir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(".henshin");
+			}
+		});
+
+        for(File file: files){
+        	
+            if(file.isDirectory())
+            	listHenshinFilesRecursively(file, henshinFiles);
+            else{
+                henshinFiles.add(file);
+            }
+        }
+        
+    }
+	
+	
 
 }
