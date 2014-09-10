3496257cf3f8cc8f0d1197a5e3ca13a5a342ea82
verenak <verena.kaefer@googlemail.com>
2014-09-10 08:29:52 +0200

    regenerated code

---
+++ de.uni_stuttgart.iste.cowolf.model.sequence_diagram.tests/src/de/uni_stuttgart/iste/cowolf/model/sequence_diagram/emf/tests/SdInteractionTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package de.uni_stuttgart.iste.cowolf.model.sequence_diagram.emf.tests;
+
+import de.uni_stuttgart.iste.cowolf.model.sequence_diagram.SdInteraction;
+import de.uni_stuttgart.iste.cowolf.model.sequence_diagram.Sequence_diagramFactory;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Sd Interaction</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class SdInteractionTest extends TestCase {
+
+	/**
+	 * The fixture for this Sd Interaction test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected SdInteraction fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(SdInteractionTest.class);
+	}
+
+	/**
+	 * Constructs a new Sd Interaction test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public SdInteractionTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Sd Interaction test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(SdInteraction fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Sd Interaction test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected SdInteraction getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(Sequence_diagramFactory.eINSTANCE.createSdInteraction());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //SdInteractionTest
