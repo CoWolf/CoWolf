3e695854868928b08f1047e3508aa567e16779e6
verenak <verena.kaefer@googlemail.com>
2014-09-10 08:31:27 +0200

    regenerated code

---
+++ de.uni_stuttgart.iste.cowolf.model.sequence_diagram/src/de/uni_stuttgart/iste/cowolf/model/sequence_diagram/Sequence_diagramFactory.java
@@ -0,0 +1,78 @@
+/**
+ */
+package de.uni_stuttgart.iste.cowolf.model.sequence_diagram;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see de.uni_stuttgart.iste.cowolf.model.sequence_diagram.Sequence_diagramPackage
+ * @generated
+ */
+public interface Sequence_diagramFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	Sequence_diagramFactory eINSTANCE = de.uni_stuttgart.iste.cowolf.model.sequence_diagram.impl.Sequence_diagramFactoryImpl.init();
+
+	/**
+	 * Returns a new object of class '<em>Message</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Message</em>'.
+	 * @generated
+	 */
+	Message createMessage();
+
+	/**
+	 * Returns a new object of class '<em>Lifeline</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Lifeline</em>'.
+	 * @generated
+	 */
+	Lifeline createLifeline();
+
+	/**
+	 * Returns a new object of class '<em>Actor</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Actor</em>'.
+	 * @generated
+	 */
+	Actor createActor();
+
+	/**
+	 * Returns a new object of class '<em>Sd Interaction</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Sd Interaction</em>'.
+	 * @generated
+	 */
+	SdInteraction createSdInteraction();
+
+	/**
+	 * Returns a new object of class '<em>Package</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Package</em>'.
+	 * @generated
+	 */
+	Package createPackage();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	Sequence_diagramPackage getSequence_diagramPackage();
+
+} //Sequence_diagramFactory
