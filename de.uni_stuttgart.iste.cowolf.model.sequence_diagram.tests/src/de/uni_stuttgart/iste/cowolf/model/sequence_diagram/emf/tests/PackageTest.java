33260b8cf99dfd8bb19e3cbc19f1aa798568dd89
verenak <verena.kaefer@googlemail.com>
2014-09-10 08:29:52 +0200

    regenerated code

---
+++ de.uni_stuttgart.iste.cowolf.model.sequence_diagram.tests/src/de/uni_stuttgart/iste/cowolf/model/sequence_diagram/emf/tests/PackageTest.java
@@ -0,0 +1,88 @@
+/**
+ */
+package de.uni_stuttgart.iste.cowolf.model.sequence_diagram.emf.tests;
+
+import de.uni_stuttgart.iste.cowolf.model.sequence_diagram.Sequence_diagramFactory;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Package</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class PackageTest extends TestCase {
+
+	/**
+	 * The fixture for this Package test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected de.uni_stuttgart.iste.cowolf.model.sequence_diagram.Package fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(PackageTest.class);
+	}
+
+	/**
+	 * Constructs a new Package test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public PackageTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Package test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(de.uni_stuttgart.iste.cowolf.model.sequence_diagram.Package fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Package test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected de.uni_stuttgart.iste.cowolf.model.sequence_diagram.Package getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(Sequence_diagramFactory.eINSTANCE.createPackage());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //PackageTest
