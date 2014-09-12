a8c52ab9795a33d78f0e5f83ae2b4bac95186362
Michael Müller <michael.mueller.90@gmail.com>
2014-09-12 14:12:38 +0200

    updated transformation rules, added some editrules for loop transitions , refs #270

--- de.uni_stuttgart.iste.cowolf.evolution.statechart.difference.technical/src/de/uni_stuttgart/iste/cowolf/evolution/statechart/difference/technical/TechnicalDifferenceBuilderStatechart.java
+++ de.uni_stuttgart.iste.cowolf.evolution.statechart.difference.technical/src/de/uni_stuttgart/iste/cowolf/evolution/statechart/difference/technical/TechnicalDifferenceBuilderStatechart.java
@@ -10,6 +10,7 @@ import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.henshin.trace.impl.TracePackageImpl;
 import org.sidiff.difference.technical.TechnicalDifferenceBuilder;
+
 import de.uni_stuttgart.iste.cowolf.model.statechart.StatemachinePackage;
 
 
