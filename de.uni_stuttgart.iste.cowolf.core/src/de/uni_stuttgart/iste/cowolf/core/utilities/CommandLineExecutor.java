f56e8c10789512ce5230ccc71036eb67dc395c05
ReneTrefft <trefftre@studi.informatik.uni-stuttgart.de>
2014-09-15 16:41:38 +0200

    cleaned manifests; javadoc

--- de.uni_stuttgart.iste.cowolf.core/src/de/uni_stuttgart/iste/cowolf/core/utilities/CommandLineExecutor.java
+++ de.uni_stuttgart.iste.cowolf.core/src/de/uni_stuttgart/iste/cowolf/core/utilities/CommandLineExecutor.java
@@ -6,62 +6,27 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
+/**
+ * 
+ * @author Rene Trefft
+ *
+ */
 public class CommandLineExecutor {
 
-	// private static InputStream commandOutput = null;
-
-//	private BufferedReader bufferedReader = null;
-//
-//	/**
-//	 * SingletonHolder is loaded on the first execution of
-//	 * CommandLineExecutor.getInstance(), not before.
-//	 */
-//	private static class SingletonHolder {
-//		private static final CommandLineExecutor INSTANCE = new CommandLineExecutor();
-//	}
-//
-//	public static CommandLineExecutor getInstance() {
-//		return SingletonHolder.INSTANCE;
-//	}
-//
-//	private CommandLineExecutor() {
-//	}
-
-//	private Set<IPrinter> commandOutputListeners = new HashSet<IPrinter>();
-//
-//	public void addListener(IPrinter commandOutputListener) {
-//		commandOutputListeners.add(commandOutputListener);
-//	}
-//
-//	private void notifyCommandOuputListeners(String message) {
-//		for (IPrinter commandOutputListener : commandOutputListeners) {
-//			commandOutputListener.println(message);
-//		}
-//	}
-
-//	private BufferedReader getCommandOutputBufferedReader() {
-//		return bufferedReader;
-//	}
-//
-//	private void setCommandOutputBufferedReader(BufferedReader bufferedReader) {
-//		this.bufferedReader = bufferedReader;
-//	}
-
-	public static void execCommandAndDiscardOutput(String directory, String command)
-			throws Exception {
+	public static void execCommandAndDiscardOutput(String directory,
+			String command) throws Exception {
 		InputStream inputStream = execCommandAndGetOutput(directory, command);
 		Reader r = new InputStreamReader(inputStream);
 		BufferedReader in = new BufferedReader(r);
-		while (in.readLine() != null) { /*
-													 * Read output until process
-													 * finished.
-													 */
+		while (in.readLine() != null) {
+			/* Read output until process finished. */
 		}
-		// in.close();
+
 	}
-	
-	public static InputStream execCommandAndGetOutput(String directory, String command) throws Exception {
-		
+
+	public static InputStream execCommandAndGetOutput(String directory,
+			String command) throws Exception {
+
 		File dir = new File(directory);
 		Process process;
 		if (isWindowsSystem()) {
@@ -72,73 +37,10 @@ public class CommandLineExecutor {
 			throw new Exception(
 					Messages.commandLineExecutor_unknown_operating_system);
 		}
-		
+
 		return process.getInputStream();
 
-//		Reader r = new InputStreamReader(process.getInputStream());
-//		bufferedReader = new BufferedReader(r);
-		
 	}
-	
-
-//	public void execCommandAndStoreOutput(String directory, String command)
-//			throws Exception {
-//		File dir = new File(directory);
-//		Process process;
-//		if (isWindowsSystem()) {
-//			process = Runtime.getRuntime().exec("cmd /c " + command, null, dir); //$NON-NLS-1$
-//		} else if (isLinuxSystem()) {
-//			process = Runtime.getRuntime().exec("./" + command, null, dir);
-//		} else {
-//			throw new Exception(
-//					Messages.commandLineExecutor_unknown_operating_system);
-//		}
-//
-//		Reader r = new InputStreamReader(process.getInputStream());
-//		bufferedReader = new BufferedReader(r);
-//
-//		setCommandOutputBufferedReader(bufferedReader);
-//
-//		// return process.getInputStream();
-//
-//	}
-
-//	public String getCommandOutputNextLine() {
-//		// if (getCommandOutput() == null) {
-//		// return null;
-//		// }
-//
-//		BufferedReader bufferedReader = getCommandOutputBufferedReader();
-//
-//		if (bufferedReader == null) {
-//			// Reader r = new InputStreamReader(getCommandOutput());
-//			// bufferedReader = new BufferedReader(r);
-//			return null;
-//		}
-//
-//		String nextLine = null;
-//
-//		try {
-//			nextLine = bufferedReader.readLine();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//
-//		if (nextLine == null) {
-//			try {
-//				bufferedReader.close();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			} finally {
-//				setCommandOutputBufferedReader(null);
-//			}
-//		} else {
-//			notifyCommandOuputListeners(nextLine);
-//		}
-//
-//		return nextLine;
-//
-//	}
 
 	private static boolean isWindowsSystem() {
 		String osName = System.getProperty("os.name").toLowerCase(); //$NON-NLS-1$
--- de.uni_stuttgart.iste.cowolf.core/src/de/uni_stuttgart/iste/cowolf/core/utilities/PrinterRegistry.java
+++ de.uni_stuttgart.iste.cowolf.core/src/de/uni_stuttgart/iste/cowolf/core/utilities/PrinterRegistry.java
@@ -3,6 +3,13 @@ package de.uni_stuttgart.iste.cowolf.core.utilities;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 
+ * Manages the registered printers.
+ * 
+ * @author Rene Trefft
+ *
+ */
 public class PrinterRegistry {
 
 	/**
@@ -20,15 +27,39 @@ public class PrinterRegistry {
 	private PrinterRegistry() {
 	}
 
+	/**
+	 * Registered printers.
+	 */
 	private Set<IPrinter> printers = new HashSet<IPrinter>();
 
+	/**
+	 * Registers a new printer.
+	 * 
+	 * @param printer to register
+	 */
 	public void addListener(IPrinter printer) {
 		printers.add(printer);
 	}
 
+	/**
+	 * Prints the line {@code printLine} and terminates the line with the Unix line
+	 * separator with all registered printers.
+	 * 
+	 * @param printName - description of the print job
+	 * @param printLine to print
+	 */
 	public void println(String printName, String printMessage) {
 		for (IPrinter printer : printers) {
 			printer.println(printName, printMessage);
+		}
+	}
+	
+	/**
+	 * Closes all printers. Mostly it consists of the closing of opened streams.
+	 */
+	public void close() {
+		for (IPrinter printer : printers) {
+			printer.close();
 		}
 	}
 
