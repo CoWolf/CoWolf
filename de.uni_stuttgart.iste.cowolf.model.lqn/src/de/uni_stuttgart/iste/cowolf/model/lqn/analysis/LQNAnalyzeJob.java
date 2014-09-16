69a9d55a92d1d2a0cb58cf0020aed373cfc4ab8b
mborja <alejo.borja@gmail.com>
2014-09-16 18:09:55 +0200

    LQN Analyze writer and others

--- de.uni_stuttgart.iste.cowolf.model.lqn/src/de/uni_stuttgart/iste/cowolf/model/lqn/analysis/LQNAnalyzeJob.java
+++ de.uni_stuttgart.iste.cowolf.model.lqn/src/de/uni_stuttgart/iste/cowolf/model/lqn/analysis/LQNAnalyzeJob.java
@@ -1,11 +1,7 @@
 package de.uni_stuttgart.iste.cowolf.model.lqn.analysis;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,10 +10,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.ecore.EObject;
@@ -26,7 +20,6 @@ import org.eclipse.emf.ecore.resource.Resource;
 import de.uni_stuttgart.iste.cowolf.core.utilities.CommandLineExecutor;
 import de.uni_stuttgart.iste.cowolf.model.IAnalysisListener;
 import de.uni_stuttgart.iste.cowolf.model.LqnCore.ActivityType;
-import de.uni_stuttgart.iste.cowolf.model.LqnCore.DocumentRoot;
 import de.uni_stuttgart.iste.cowolf.model.LqnCore.EntryType;
 import de.uni_stuttgart.iste.cowolf.model.LqnCore.ProcessorType;
 import de.uni_stuttgart.iste.cowolf.model.LqnCore.TaskType;
@@ -42,17 +35,12 @@ public class LQNAnalyzeJob extends Job {
 	private final String OUTPUT_NAME = "model";
 	private final String OUTPUT_FOLDER_NAME = OUTPUT_NAME+".d";
 	private final String OUTPUT_FILE_NAME = OUTPUT_NAME+".lqxo";
-	private final static Map<String,LQNSolverFileGenerator> lqnFilesGenerator;
+	private final String PATH_TO_INPUT_LQN_FILE = System.getProperty("java.io.tmpdir") + INPUT_FILE_NAME;
+	private final String PATH_TO_OUTPUT_LQN_FILE = System.getProperty("java.io.tmpdir") + OUTPUT_FOLDER_NAME +"/" + OUTPUT_FILE_NAME;
+
 	private final String lqnSolverBasicCommand = "lqns.exe -x ";
 	private Map<String,String> mapIdName;
 
-	private static final String CRLF = "\r\n";
-	
-	static{
-		lqnFilesGenerator = new HashMap<String, LQNSolverFileGenerator>();
-		lqnFilesGenerator.put("serviceTime", new LQNSolverFileStandardGenerator());
-	}
-	
 	public LQNAnalyzeJob(final Resource model,
 			final Map<String, Object> parameters, IAnalysisListener listener) {
 		super("LQN Analysis Job");
@@ -64,15 +52,13 @@ public class LQNAnalyzeJob extends Job {
 
 	@Override
 	protected IStatus run(IProgressMonitor monitor) {
+		
 		final String pathToLQNSolver = (String) parameters.get(LQNModelManager.PARAM_PATH_TO_LQN_SOLVER);
-		final String pathToInputLQNFile = System.getProperty("java.io.tmpdir") + INPUT_FILE_NAME;
-		final String pathToOutputLQNFile = System.getProperty("java.io.tmpdir") + OUTPUT_FOLDER_NAME +"/" + OUTPUT_FILE_NAME;
-
 		monitor.beginTask("Analyze LQN Model", 2);
 
 		try {
 			// 1. Generate lqn file
-			writeTempFileFromGenerator(pathToInputLQNFile);
+			LQNAnalyzeFilesWriter.writeTempFileFromGenerator(model, PATH_TO_INPUT_LQN_FILE, parameters);
 			monitor.worked(1);
 
 			// 2. Use CommandLineExecutor to execute lqn solver.
@@ -85,7 +71,7 @@ public class LQNAnalyzeJob extends Job {
 			monitor.worked(1);
 
 			// 3. Parse the results from generated file
-			parseResultFile(pathToOutputLQNFile);
+			parseResultFile(PATH_TO_OUTPUT_LQN_FILE);
 			monitor.done();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -111,57 +97,24 @@ public class LQNAnalyzeJob extends Job {
 		}
 		br.close();
 	}
+	
+	protected void writeToResultFile(){
+//		StringBuilder sb = new StringBuilder("");
+//		if (results != null) {
+//			for (String string : results) {
+//				sb.append(string);
+//			}
+//		}
+//		IFile resultFile = LQNFilesWriter.writeToResultFile(model, results);
+		
+		IFile resultFile = LQNAnalyzeFilesWriter.generateHTMLFile(model,PATH_TO_OUTPUT_LQN_FILE);
 
-	protected void writeTempFileFromGenerator(String pathToOutputLQNFile)
-			throws Exception {
-		LQNSolverFileGenerator generator = lqnFilesGenerator.get((parameters.get("typeOfAnalysis")));
-		OutputStreamWriter out = new FileWriter(pathToOutputLQNFile);
-		out.write(generator.doGenerateLQNSolverFile(
-				(DocumentRoot) model.getContents().get(0), parameters).toString());
-		out.flush();
-		out.close();
-	}
-
-	public void writeToResultFile() {
-		Resource resource = model;
-
-		IFile modelfile = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(resource.getURI().toPlatformString(true)));
-
-		IFile resultfile = ResourcesPlugin
-				.getWorkspace()
-				.getRoot()
-				.getFile(
-						modelfile.getFullPath()
-								.addFileExtension("lqnAnalysis.xml"));
-		OutputStreamWriter out = null;
-		try {
-			out = new FileWriter(resultfile.getLocation().toOSString());
-
-			StringBuilder sb = new StringBuilder();
-
-			for (String line : results) {
-				sb.append(line).append(CRLF);
-			}
-
-			out.write(sb.toString());
-
-			if (this.listener != null) {
-				this.listener.finished(resource, resultfile);
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				out.flush();
-				out.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
+		if (this.listener != null) {
+			this.listener.finished(model, resultFile);
 		}
 	}
 
+	@SuppressWarnings("all")
 	private void generateModelIdNameMap(){
 		mapIdName = new HashMap<String,String>();
 		
