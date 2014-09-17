8029b6822fd9778d9672263bd93b3434c0cbc725
Jonas Scheurich <jonasscheurich@gmx.net>
2014-09-17 13:57:59 +0200

    SDevolve - Technical Builder   -> Extension

---
+++ de.uni_stuttgart.iste.cowolf.evolution.sequence_diagram.difference.technical/src/de/uni_stuttgart/iste/cowolf/evolution/sequence_diagram/difference/technical/TechnicalDifferenceBuilderSequence_Diagram.java
@@ -0,0 +1,57 @@
+package de.uni_stuttgart.iste.cowolf.evolution.sequence_diagram.difference.technical;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.henshin.trace.impl.TracePackageImpl;
+import org.sidiff.difference.technical.TechnicalDifferenceBuilder;
+
+import de.uni_stuttgart.iste.cowolf.model.sequence_diagram.Sequence_diagramPackage;
+
+public class TechnicalDifferenceBuilderSequence_Diagram extends TechnicalDifferenceBuilder{
+
+	@Override
+	protected Set<EClass> getUnconsideredNodeTypes() {
+		Set<EClass> unconsideredNodeTypes = new HashSet<EClass>();
+		unconsideredNodeTypes.add(TracePackageImpl.eINSTANCE.getTrace());
+		return unconsideredNodeTypes;
+	}
+
+	@Override
+	protected Set<EReference> getUnconsideredEdgeTypes() {
+		Set<EReference> unconsideredEdgeTypes = new HashSet<EReference>();
+		unconsideredEdgeTypes.add(TracePackageImpl.eINSTANCE.getTrace_Source());
+		unconsideredEdgeTypes.add(TracePackageImpl.eINSTANCE
+				.getTrace_SubTraces());
+		unconsideredEdgeTypes.add(TracePackageImpl.eINSTANCE.getTrace_Target());
+		return unconsideredEdgeTypes;
+	}
+
+	@Override
+	protected Set<EAttribute> getUnconsideredAttributeTypes() {
+		Set<EAttribute> unconsideredAttributeTypes = new HashSet<EAttribute>();
+		unconsideredAttributeTypes.add(TracePackageImpl.eINSTANCE
+				.getTrace_Name());
+		return unconsideredAttributeTypes;
+	}
+
+	@Override
+	protected String getObjectName(EObject obj) {
+		return obj.toString();
+	}
+
+	@Override
+	public String getDocumentType() {
+		return Sequence_diagramPackage.eNS_URI;
+	}
+
+	@Override
+	public String getName() {
+		return "Sequence Diagram Technical Difference Builder";
+	}
+
+}
