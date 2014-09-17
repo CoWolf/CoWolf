7b4a871485eead64ba1271fd10fbd275c5b51e99
verenak <verena.kaefer@googlemail.com>
2014-09-17 14:44:22 +0200

    Editor opens but with Sirius error message

--- de.uni_stuttgart.iste.cowolf.ui.navigator/src/de/uni_stuttgart/iste/cowolf/ui/navigator/editors/GraphicalSequenceEditor.java
+++ de.uni_stuttgart.iste.cowolf.ui.navigator/src/de/uni_stuttgart/iste/cowolf/ui/navigator/editors/GraphicalSequenceEditor.java
@@ -1,72 +1,38 @@
 package de.uni_stuttgart.iste.cowolf.ui.navigator.editors;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.sirius.business.api.session.Session;
-import org.eclipse.sirius.business.api.session.SessionManager;
-import org.eclipse.sirius.ui.business.api.dialect.DialectUIManager;
-import org.eclipse.sirius.viewpoint.DAnalysis;
-import org.eclipse.sirius.viewpoint.DRepresentation;
-import org.eclipse.sirius.viewpoint.DView;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.MultiPageEditorPart;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.sirius.diagram.ui.tools.api.editor.AbstractSpecificDDiagramEditor;
+import org.eclipse.sirius.ui.business.api.editor.SpecificEditorInputTranformer;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.PartInitException;
 
-public class GraphicalSequenceEditor extends MultiPageEditorPart {
+public class GraphicalSequenceEditor extends AbstractSpecificDDiagramEditor {
 
-
-	@Override
-	protected void createPages() {
-		// TODO Auto-generated method stub
-		IWorkbenchWindow window = PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow();
-		IStructuredSelection selection = (IStructuredSelection) window
-				.getSelectionService().getSelection();
-
-		// get the .aird file and the corresponding Sirius Session
-		IFile selectedFile = (IFile) selection.getFirstElement();
-
-		// create viewpoint
-					IFile airdFile = selectedFile.getProject().getFile(
-							"representations.aird");
-					URI myRepresentationsFileURI = URI.createPlatformResourceURI(airdFile
-							.getFullPath().toOSString(), true);
-
-		Session siriusSession = SessionManager.INSTANCE.getSession(
-				myRepresentationsFileURI, new NullProgressMonitor());
-
-		// get the DRepresentation to open
-		DAnalysis root = (DAnalysis) siriusSession.getSessionResource()
-				.getContents().get(0);
-		DView dView = root.getOwnedViews().get(0);
-		DRepresentation myRepresentation = dView.getOwnedRepresentations().get(
-				0);
-
-		// open representation werden wohl die
-		DialectUIManager.INSTANCE.openEditor(siriusSession, myRepresentation,
-				new NullProgressMonitor());
-
+	 private Transformer util = new Transformer();
+	
+	public String getDiagramDescriptionName() {
+		return "Sequence Diagram";
 	}
 
-	@Override
-	public void doSave(IProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-		
+	public URI getViewpointURI() {
+		return URI
+				.createURI("viewpoint:/de.uni_stuttgart.iste.cowolf.model.sequence_diagram.sirius.editor/Sequence%20diagram");
 	}
 
-	@Override
-	public void doSaveAs() {
-		// TODO Auto-generated method stub
-		
+	public boolean isSessionStoredInWorkspace() {
+		return true;
 	}
-
-	@Override
-	public boolean isSaveAsAllowed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	
+	 @Override
+	    public void init(final IEditorSite site, final IEditorInput input) throws PartInitException {
+	        util.init(getViewpointURI(), getDiagramDescriptionName());
+	        super.init(site, util.transformInput(input, getSelection(site), isSessionStoredInWorkspace()));
+	    }
+	 
+	 private ISelection getSelection(IWorkbenchPartSite site) {
+	        return site.getWorkbenchWindow().getSelectionService().getSelection();
+	    }
 }
