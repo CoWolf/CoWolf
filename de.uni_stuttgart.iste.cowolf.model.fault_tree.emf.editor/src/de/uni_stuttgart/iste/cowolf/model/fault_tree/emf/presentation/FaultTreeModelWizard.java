c68be9f98f6569411227ecfebf5a476827e5f9f3
mborja <alejo.borja@gmail.com>
2014-09-19 22:24:01 +0200

    Merge fix

--- de.uni_stuttgart.iste.cowolf.model.fault_tree.emf.editor/src/de/uni_stuttgart/iste/cowolf/model/fault_tree/emf/presentation/FaultTreeModelWizard.java
+++ de.uni_stuttgart.iste.cowolf.model.fault_tree.emf.editor/src/de/uni_stuttgart/iste/cowolf/model/fault_tree/emf/presentation/FaultTreeModelWizard.java
@@ -14,19 +14,6 @@ import java.util.MissingResourceException;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-import org.eclipse.emf.common.CommonPlugin;
-import org.eclipse.emf.common.command.Command;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EClassifier;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.xmi.XMLResource;
-import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
-import org.eclipse.emf.transaction.RecordingCommand;
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -34,9 +21,24 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.CommonPlugin;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.edit.ui.provider.ExtendedImageRegistry;
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.sirius.business.api.dialect.DialectManager;
@@ -53,8 +55,8 @@ import org.eclipse.sirius.viewpoint.DRepresentation;
 import org.eclipse.sirius.viewpoint.description.RepresentationDescription;
 import org.eclipse.sirius.viewpoint.description.Viewpoint;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
@@ -62,22 +64,16 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.WorkspaceModifyOperation;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
-import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.part.ISetSelectionTarget;
 
 import de.uni_stuttgart.iste.cowolf.model.fault_tree.FaultTreeFactory;
 import de.uni_stuttgart.iste.cowolf.model.fault_tree.FaultTreePackage;
 import de.uni_stuttgart.iste.cowolf.model.fault_tree.emf.provider.FaultTreeEditPlugin;
-
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
 
 /**
  * This is a simple wizard for creating a new model file.
@@ -204,10 +200,10 @@ public class FaultTreeModelWizard extends Wizard implements INewWizard {
 	}
 
 	/**
-	 * Do the work after everything is specified.
-	 * <!-- begin-user-doc --> <!--
+	 * Do the work after everything is specified. <!-- begin-user-doc --> <!--
 	 * end-user-doc -->
-	 * @generated
+	 * 
+	 * @generated not
 	 */
 	@Override
 	public boolean performFinish() {
@@ -218,77 +214,154 @@ public class FaultTreeModelWizard extends Wizard implements INewWizard {
 
 			// Do the work within an operation.
 			//
-			WorkspaceModifyOperation operation =
-				new WorkspaceModifyOperation() {
-					@Override
-					protected void execute(IProgressMonitor progressMonitor) {
-						try {
-							// Create a resource set
-							//
-							ResourceSet resourceSet = new ResourceSetImpl();
+			WorkspaceModifyOperation operation = new WorkspaceModifyOperation() {
+				@Override
+				protected void execute(IProgressMonitor progressMonitor) {
+					try {
+						// Create a resource set
+						//
+						ResourceSet resourceSet = new ResourceSetImpl();
 
-							// Get the URI of the model file.
-							//
-							URI fileURI = URI.createPlatformResourceURI(modelFile.getFullPath().toString(), true);
+						// Get the URI of the model file.
+						//
+						URI fileURI = URI.createPlatformResourceURI(modelFile
+								.getFullPath().toString(), true);
 
-							// Create a resource for this file.
-							//
-							Resource resource = resourceSet.createResource(fileURI);
+						// Create a resource for this file.
+						//
+						Resource resource = resourceSet.createResource(fileURI);
 
-							// Add the initial model object to the contents.
-							//
-							EObject rootObject = createInitialModel();
-							if (rootObject != null) {
-								resource.getContents().add(rootObject);
-							}
-
-							// Save the contents of the resource to the file system.
-							//
-							Map<Object, Object> options = new HashMap<Object, Object>();
-							options.put(XMLResource.OPTION_ENCODING, initialObjectCreationPage.getEncoding());
-							resource.save(options);
+						// Add the initial model object to the contents.
+						//
+						EObject rootObject = createInitialModel();
+						if (rootObject != null) {
+							resource.getContents().add(rootObject);
 						}
-						catch (Exception exception) {
-							FaultTreeEditorPlugin.INSTANCE.log(exception);
-						}
-						finally {
-							progressMonitor.done();
-						}
+
+						// Save the contents of the resource to the file system.
+						//
+						Map<Object, Object> options = new HashMap<Object, Object>();
+						options.put(XMLResource.OPTION_ENCODING,
+								initialObjectCreationPage.getEncoding());
+						resource.save(options);
+					} catch (Exception exception) {
+						FaultTreeEditorPlugin.INSTANCE.log(exception);
+					} finally {
+						progressMonitor.done();
 					}
-				};
+				}
+			};
 
 			getContainer().run(false, false, operation);
 
 			// Select the new file resource in the current view.
 			//
-			IWorkbenchWindow workbenchWindow = workbench.getActiveWorkbenchWindow();
+			IWorkbenchWindow workbenchWindow = workbench
+					.getActiveWorkbenchWindow();
 			IWorkbenchPage page = workbenchWindow.getActivePage();
 			final IWorkbenchPart activePart = page.getActivePart();
 			if (activePart instanceof ISetSelectionTarget) {
-				final ISelection targetSelection = new StructuredSelection(modelFile);
-				getShell().getDisplay().asyncExec
-					(new Runnable() {
-						 public void run() {
-							 ((ISetSelectionTarget)activePart).selectReveal(targetSelection);
-						 }
-					 });
+				final ISelection targetSelection = new StructuredSelection(
+						modelFile);
+				getShell().getDisplay().asyncExec(new Runnable() {
+					public void run() {
+						((ISetSelectionTarget) activePart)
+								.selectReveal(targetSelection);
+					}
+				});
 			}
 
-			// Open an editor on the new file.
-			//
-			try {
-				page.openEditor
-					(new FileEditorInput(modelFile),
-					 workbench.getEditorRegistry().getDefaultEditor(modelFile.getFullPath().toString()).getId());					 	 
-			}
-			catch (PartInitException exception) {
-				MessageDialog.openError(workbenchWindow.getShell(), FaultTreeEditorPlugin.INSTANCE.getString("_UI_OpenEditorError_label"), exception.getMessage());
-				return false;
-			}
+			// create viewpoint
+			IFile airdFile = modelFile.getProject().getFile(
+					"representations.aird");
+			if (!airdFile.exists())
+				throw new Exception("could not found file:"
+						+ airdFile.getLocationURI());
+			URI airdFileURI = URI.createPlatformResourceURI(airdFile
+					.getFullPath().toOSString(), true);
+			Session session = SessionManager.INSTANCE.getSession(airdFileURI,
+					new NullProgressMonitor());
+
+			URI fileURI = URI.createPlatformResourceURI(modelFile.getFullPath()
+					.toString(), true);
+
+			// adding the resource also to Sirius session
+			AddSemanticResourceCommand addCommandToSession = new AddSemanticResourceCommand(
+					session, fileURI, new NullProgressMonitor());
+			session.getTransactionalEditingDomain().getCommandStack()
+					.execute(addCommandToSession);
+			session.save(new NullProgressMonitor());
+
+			;
+
+			// find and add viewpoint
+			Set<Viewpoint> availableViewpoints = ViewpointSelection
+					.getViewpoints(FILE_EXTENSION);
+
+			if (availableViewpoints.isEmpty())
+				throw new Exception(
+						"Could not find viewport for fileextension "
+								+ FILE_EXTENSION);
+
+			Set<Viewpoint> viewpoints = new HashSet<Viewpoint>();
+			for (Viewpoint p : availableViewpoints)
+				viewpoints.add(SiriusResourceHelper.getCorrespondingViewpoint(
+						session, p));
+
+			ViewpointSelection.Callback callback = new ViewpointSelectionCallbackWithConfimation();
+
+			RecordingCommand command = new ChangeViewpointSelectionCommand(
+					session, callback, viewpoints, new HashSet<Viewpoint>(),
+					true, new NullProgressMonitor());
+			TransactionalEditingDomain domain = session
+					.getTransactionalEditingDomain();
+			domain.getCommandStack().execute(command);
+
+			// create representation
+			Object[] elements1 = session.getSemanticResources().toArray();
+			Resource resource = (Resource) elements1[elements1.length - 1];
+
+			
+
+			EObject rootObject = resource.getContents().get(0);
+
+
+			Collection<RepresentationDescription> descriptions = DialectManager.INSTANCE
+					.getAvailableRepresentationDescriptions(
+							session.getSelectedViewpoints(false), rootObject);
+			if (descriptions.isEmpty())
+				throw new Exception(
+						"Could not find representation description for object: "
+								+ rootObject);
+			RepresentationDescription description = descriptions.iterator()
+					.next();
+
+			DialectManager viewpointDialectManager = DialectManager.INSTANCE;
+			Command createViewCommand = new CreateRepresentationCommand(
+					session, description, rootObject, modelFile.getName(),
+					new NullProgressMonitor());
+
+			session.getTransactionalEditingDomain().getCommandStack()
+					.execute(createViewCommand);
+
+			SessionManager.INSTANCE.notifyRepresentationCreated(session);
+
+			// open editor for last representation
+			Collection<DRepresentation> representations = viewpointDialectManager
+					.getRepresentations(description, session);
+			Object[] arrayRep = representations.toArray();
+			DRepresentation myDiagramRepresentation = (DRepresentation) arrayRep[arrayRep.length - 1];
+			DialectUIManager dialectUIManager = DialectUIManager.INSTANCE;
+			dialectUIManager.openEditor(session, myDiagramRepresentation,
+					new NullProgressMonitor());
+
+			// save session and refresh workspace
+			session.save(new NullProgressMonitor());
+			modelFile.getProject().refreshLocal(IResource.DEPTH_INFINITE,
+					new NullProgressMonitor());
 
 			return true;
-		}
-		catch (Exception exception) {
+		} catch (Exception exception) {
 			FaultTreeEditorPlugin.INSTANCE.log(exception);
 			return false;
 		}
