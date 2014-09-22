961dd505423a3fbbfd3bffd6ca34b8e1c42a3911
verenak <verena.kaefer@googlemail.com>
2014-09-22 09:23:20 +0200

    added sirius plugin

---
+++ de.uni_stuttgart.iste.cowolf.model.component_diagram.sirius.editor/src/de/uni_stuttgart/iste/cowolf/model/component_diagram/sirius/editor/design/UMLDesignerPlugin.java
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package de.uni_stuttgart.iste.cowolf.model.component_diagram.sirius.editor.design;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.runtime.Status;
+import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
+import org.eclipse.sirius.viewpoint.description.Viewpoint;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle.
+ * 
+ * @author Gonzague Reydet <a href="mailto:gonzague.reydet@obeo.fr">gonzague.reydet@obeo.fr</a>
+ */
+public class UMLDesignerPlugin extends AbstractUIPlugin {
+
+	/**
+	 * The plug-in ID.
+	 */
+	public static final String PLUGIN_ID = "org.obeonetwork.dsl.uml2.design";
+
+	/**
+	 * The shared instance.
+	 */
+	private static UMLDesignerPlugin plugin;
+
+	/**
+	 * The {@link Viewpoint}s registered in this plug-in.
+	 */
+	private static Set<Viewpoint> viewpoints;
+
+	/**
+	 * The constructor.
+	 */
+	public UMLDesignerPlugin() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		viewpoints = new HashSet<Viewpoint>();
+		viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(
+				PLUGIN_ID + "/description/uml2.odesign"));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		if (viewpoints != null) {
+			for (final Viewpoint viewpoint : viewpoints) {
+				ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
+			}
+			viewpoints.clear();
+			viewpoints = null;
+		}
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance.
+	 * 
+	 * @return the shared instance
+	 */
+	public static UMLDesignerPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * A helper to log plugin errors.
+	 * 
+	 * @param severity
+	 *            the error severity.
+	 * @param message
+	 *            the error message.
+	 * @param exception
+	 *            the error exception.
+	 */
+	public static void log(int severity, String message, Throwable exception) {
+		getDefault().getLog().log(new Status(severity, PLUGIN_ID, message, exception));
+	}
+}
