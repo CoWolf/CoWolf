4e9dd11ffddc531f78534bf25709bdcad68b290d
Jonas Scheurich <jonasscheurich@gmx.net>
2014-09-23 10:25:16 +0200

    Sequence Diagram: Manager created

--- de.uni_stuttgart.iste.cowolf.evolution.sequence_diagram/src/de/uni_stuttgart/iste/cowolf/evolution/sequence_diagram/Sequence_DiagramEvolutionManager.java
+++ de.uni_stuttgart.iste.cowolf.evolution.sequence_diagram/src/de/uni_stuttgart/iste/cowolf/evolution/sequence_diagram/Sequence_DiagramEvolutionManager.java
@@ -1,26 +1,14 @@
 package de.uni_stuttgart.iste.cowolf.evolution.sequence_diagram;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
 
 import de.uni_stuttgart.iste.cowolf.evolution.AbstractEvolutionManager;
 import de.uni_stuttgart.iste.cowolf.evolution.EvolutionTypeInfo;
 import de.uni_stuttgart.iste.cowolf.model.sequence_diagram.Package;
 
 public class Sequence_DiagramEvolutionManager extends AbstractEvolutionManager{
-	
-	@Override
-	public boolean isManaged(Resource model) {
-		if (model == null || model.getContents() == null
-				|| model.getContents().isEmpty()) {
-			return false;
-		}
-
-		return model.getContents().get(0) instanceof Package;
-	}
 
 	@Override
-	protected Class<? extends EObject> getManagedClass() {
+	protected Class<?> getManagedClass() {
 		return Package.class;
 	}
 
