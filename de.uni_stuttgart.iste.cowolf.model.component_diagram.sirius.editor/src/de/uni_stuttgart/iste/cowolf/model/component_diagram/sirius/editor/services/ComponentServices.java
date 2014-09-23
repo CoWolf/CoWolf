98969c14b5d4f699d0bfee8248d91bc48f0610f4
verenak <verena.kaefer@googlemail.com>
2014-09-23 16:34:44 +0200

    connectors are now shown

---
+++ de.uni_stuttgart.iste.cowolf.model.component_diagram.sirius.editor/src/de/uni_stuttgart/iste/cowolf/model/component_diagram/sirius/editor/services/ComponentServices.java
@@ -0,0 +1,47 @@
+package de.uni_stuttgart.iste.cowolf.model.component_diagram.sirius.editor.services;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+
+import de.uni_stuttgart.iste.cowolf.model.component_diagram.Architecture;
+import de.uni_stuttgart.iste.cowolf.model.component_diagram.ComponentInstance;
+import de.uni_stuttgart.iste.cowolf.model.component_diagram.Connector;
+import de.uni_stuttgart.iste.cowolf.model.component_diagram.PortInstance;
+
+public class ComponentServices {
+
+	public static EObject getRootElelent(PortInstance port) {
+		ComponentInstance component = (ComponentInstance)port.eContainer();
+		EObject container = component.eContainer();
+		while (!(container.eContainer() instanceof Architecture)) {
+			container = container.eContainer();
+		}
+		return container;
+	}
+
+	public static EObject findSource(Connector connector) {
+		EList<PortInstance> ports = connector.getPort();
+		for(PortInstance port : ports){
+			if(port.getOutComponent() != null){
+				return port;
+			}
+		}
+		
+
+		return null;
+	}
+	
+	public static EObject findTarget(Connector connector) {
+		EList<PortInstance> ports = connector.getPort();
+		for(PortInstance port : ports){
+			if(port.getInComponent() != null){
+				return port;
+			}
+		}
+		
+
+		return null;
+	}
+
+	
+}
