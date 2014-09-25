a1c6f77ca1fbbb838e12abc819209d66a1dca0ac
zimmerml <zimmerml@users.noreply.github.com>
2014-09-25 11:39:29 +0200

    use date of last version for labeling ref #368

--- de.uni_stuttgart.iste.cowolf.ui.transformation/src/de/uni_stuttgart/iste/cowolf/ui/transformation/wizard/ModelVersionTreeLabelProvider.java
+++ de.uni_stuttgart.iste.cowolf.ui.transformation/src/de/uni_stuttgart/iste/cowolf/ui/transformation/wizard/ModelVersionTreeLabelProvider.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.swt.graphics.Image;
@@ -17,6 +18,7 @@ import de.uni_stuttgart.iste.cowolf.core.ModelAssociation.Association;
 import de.uni_stuttgart.iste.cowolf.core.ModelAssociation.Model;
 import de.uni_stuttgart.iste.cowolf.core.ModelAssociation.ModelAssociation;
 import de.uni_stuttgart.iste.cowolf.core.ModelAssociation.ModelAssociationFactory;
+import de.uni_stuttgart.iste.cowolf.core.ModelAssociation.ModelVersion;
 
 public class ModelVersionTreeLabelProvider implements ILabelProvider {
 	
@@ -83,7 +85,9 @@ public class ModelVersionTreeLabelProvider implements ILabelProvider {
 				return iFile.getName();
 			}
 			
-			Date date = new Date(assoc.getTarget().get(0).getTimestamp());
+			EList<ModelVersion> versions = model.getVersions();
+			
+			Date date = new Date(versions.get(versions.size()-1).getTimestamp());
 			DateFormat df = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
 			return iFile.getName() + " (" + df.format(date) + ")";
 		}
