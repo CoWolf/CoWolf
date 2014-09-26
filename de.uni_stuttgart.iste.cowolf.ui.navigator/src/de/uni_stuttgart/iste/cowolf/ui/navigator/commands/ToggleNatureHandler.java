b2920aa8193a8bf421c610690a9e492e81378c42
Tim Sanwald <sanwald.tim@googlemail.com>
2014-09-26 12:38:03 +0200

    Added menu contribution

---
+++ de.uni_stuttgart.iste.cowolf.ui.navigator/src/de/uni_stuttgart/iste/cowolf/ui/navigator/commands/ToggleNatureHandler.java
@@ -0,0 +1,14 @@
+package de.uni_stuttgart.iste.cowolf.ui.navigator.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ToggleNatureHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.out.println("Toggle Nature");
+        return null;
+    }
+    
+}
