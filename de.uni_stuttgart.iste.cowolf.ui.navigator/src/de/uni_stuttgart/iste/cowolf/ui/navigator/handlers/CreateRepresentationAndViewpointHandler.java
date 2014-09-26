01842dd6baf4e0cb6353459f712cdc91a8d490cd
verenak <verena.kaefer@googlemail.com>
2014-09-26 11:23:01 +0200

    Rename of aird file works

--- de.uni_stuttgart.iste.cowolf.ui.navigator/src/de/uni_stuttgart/iste/cowolf/ui/navigator/handlers/CreateRepresentationAndViewpointHandler.java
+++ de.uni_stuttgart.iste.cowolf.ui.navigator/src/de/uni_stuttgart/iste/cowolf/ui/navigator/handlers/CreateRepresentationAndViewpointHandler.java
@@ -5,12 +5,20 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
+import javax.swing.JOptionPane;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -63,102 +71,148 @@ public class CreateRepresentationAndViewpointHandler extends AbstractHandler {
 			createAll(file, file.getFileExtension());
 		}
 
-
 		return null;
 	}
 
 	public static void createAll(IFile modelFile, String fileExtension) {
 		try {
-			// Now we have to create an aird file
-			URI airdFileURI = URI.createPlatformResourceURI(modelFile
-					.getFullPath().toString() + ".aird", true);
-
-			// Create a Session from the session model URI
-			org.eclipse.sirius.business.api.session.SessionCreationOperation sessionCreationOperation = new DefaultLocalSessionCreationOperation(
-					airdFileURI, new NullProgressMonitor());
-			sessionCreationOperation.execute();
-
-			// create viewpoint
-			Session session = SessionManager.INSTANCE.getSession(airdFileURI,
-					new NullProgressMonitor());
-
-			URI fileURI = URI.createPlatformResourceURI(modelFile.getFullPath()
-					.toString(), true);
-
-			// adding the resource to the session
-			AddSemanticResourceCommand addCommandToSession = new AddSemanticResourceCommand(
-					session, fileURI, new NullProgressMonitor());
-			session.getTransactionalEditingDomain().getCommandStack()
-					.execute(addCommandToSession);
-			session.save(new NullProgressMonitor());
-
-			// find and add viewpoint
 			Set<Viewpoint> availableViewpoints = ViewpointSelection
 					.getViewpoints(fileExtension);
-			if (availableViewpoints.isEmpty())
-				throw new Exception(
-						"Could not find viewport for file extension "
-								+ fileExtension);
+			if (availableViewpoints.isEmpty()) {
+				JOptionPane.showMessageDialog(null,
+						"There is no viewpoint for " + modelFile.getFullPath());
+			} else {
+				// Now we have to create an aird file
+				URI airdFileURI = URI.createPlatformResourceURI(modelFile
+						.getFullPath().toString() + ".aird", true);
 
-			Set<Viewpoint> viewpoints = new HashSet<Viewpoint>();
-			for (Viewpoint p : availableViewpoints)
-				viewpoints.add(SiriusResourceHelper.getCorrespondingViewpoint(
-						session, p));
+				// Create a Session from the session model URI
+				org.eclipse.sirius.business.api.session.SessionCreationOperation sessionCreationOperation = new DefaultLocalSessionCreationOperation(
+						airdFileURI, new NullProgressMonitor());
+				sessionCreationOperation.execute();
 
-			ViewpointSelection.Callback callback = new ViewpointSelectionCallbackWithConfimation();
+				// create viewpoint
+				Session session = SessionManager.INSTANCE.getSession(
+						airdFileURI, new NullProgressMonitor());
 
-			RecordingCommand command = new ChangeViewpointSelectionCommand(
-					session, callback, viewpoints, new HashSet<Viewpoint>(),
-					true, new NullProgressMonitor());
-			TransactionalEditingDomain domain = session
-					.getTransactionalEditingDomain();
-			domain.getCommandStack().execute(command);
+				URI fileURI = URI.createPlatformResourceURI(modelFile
+						.getFullPath().toString(), true);
+
+				// adding the resource to the session
+				AddSemanticResourceCommand addCommandToSession = new AddSemanticResourceCommand(
+						session, fileURI, new NullProgressMonitor());
+				session.getTransactionalEditingDomain().getCommandStack()
+						.execute(addCommandToSession);
+				session.save(new NullProgressMonitor());
+
+				// find and add viewpoint
+
+				Set<Viewpoint> viewpoints = new HashSet<Viewpoint>();
+				for (Viewpoint p : availableViewpoints)
+					viewpoints.add(SiriusResourceHelper
+							.getCorrespondingViewpoint(session, p));
+
+				ViewpointSelection.Callback callback = new ViewpointSelectionCallbackWithConfimation();
+
+				RecordingCommand command = new ChangeViewpointSelectionCommand(
+						session, callback, viewpoints,
+						new HashSet<Viewpoint>(), true,
+						new NullProgressMonitor());
+				TransactionalEditingDomain domain = session
+						.getTransactionalEditingDomain();
+				domain.getCommandStack().execute(command);
 
 			Object[] elements1 = session.getSemanticResources().toArray();
+							.toArray();
 			Resource resource = (Resource) elements1[elements1.length - 1];
 			
 			AbstractModelManager modelManager = ModelRegistry.getInstance()
 					.getModelManager(modelFile.getFileExtension());
+							.toArray();
 			
 			EObject rootObject = modelManager.getRootObject(resource);
 
-			Collection<RepresentationDescription> descriptions = DialectManager.INSTANCE
-					.getAvailableRepresentationDescriptions(
-							session.getSelectedViewpoints(false), rootObject);
-			if (descriptions.isEmpty())
-				throw new Exception(
-						"Could not find representation description for object: "
-								+ rootObject);
-			RepresentationDescription description = descriptions.iterator()
-					.next();
+				Collection<RepresentationDescription> descriptions = DialectManager.INSTANCE
+						.getAvailableRepresentationDescriptions(
+								session.getSelectedViewpoints(false),
+								rootObject);
+				if (descriptions.isEmpty())
+					throw new Exception(
+							"Could not find representation description for object: "
+									+ rootObject);
+				RepresentationDescription description = descriptions.iterator()
+						.next();
 
-			DialectManager viewpointDialectManager = DialectManager.INSTANCE;
+				DialectManager viewpointDialectManager = DialectManager.INSTANCE;
 
-			Command createViewCommand = new CreateRepresentationCommand(
-					session, description, rootObject, modelFile.getName(),
-					new NullProgressMonitor());
+				Command createViewCommand = new CreateRepresentationCommand(
+						session, description, rootObject, modelFile.getName(),
+						new NullProgressMonitor());
 
-			session.getTransactionalEditingDomain().getCommandStack()
-					.execute(createViewCommand);
+				session.getTransactionalEditingDomain().getCommandStack()
+						.execute(createViewCommand);
 
-			SessionManager.INSTANCE.notifyRepresentationCreated(session);
+				SessionManager.INSTANCE.notifyRepresentationCreated(session);
 
-			// open editor for last representation
-			Collection<DRepresentation> representations = viewpointDialectManager
-					.getRepresentations(description, session);
-			Object[] arrayRep = representations.toArray();
-			DRepresentation myDiagramRepresentation = (DRepresentation) arrayRep[arrayRep.length - 1];
-			DialectUIManager dialectUIManager = DialectUIManager.INSTANCE;
-			dialectUIManager.openEditor(session, myDiagramRepresentation,
-					new NullProgressMonitor());
+				// open editor for last representation
+				Collection<DRepresentation> representations = viewpointDialectManager
+						.getRepresentations(description, session);
+				Object[] arrayRep = representations.toArray();
+				DRepresentation myDiagramRepresentation = (DRepresentation) arrayRep[arrayRep.length - 1];
+				DialectUIManager dialectUIManager = DialectUIManager.INSTANCE;
+				dialectUIManager.openEditor(session, myDiagramRepresentation,
+						new NullProgressMonitor());
 
-			// save session and refresh workspace
-			session.save(new NullProgressMonitor());
-			modelFile.getProject().refreshLocal(IResource.DEPTH_INFINITE,
-					new NullProgressMonitor());
+				// save session and refresh workspace
+				session.save(new NullProgressMonitor());
+				modelFile.getProject().refreshLocal(IResource.DEPTH_INFINITE,
+						new NullProgressMonitor());
+			}
 		} catch (Exception e) {
 			LOGGER.error("Loading resource failed.", e);
 
 		}
 	}
+	
+	public static void renameAirdFile(final IFile source, final IPath newPath ){
+		// update aird file
+				new WorkspaceJob("Update model versioning...") {
+
+					@Override
+					public IStatus runInWorkspace(IProgressMonitor monitor) {
+						IPath oldRepresentationPath = source.getProjectRelativePath()
+								.removeLastSegments(1)
+								.append(source.getName() + ".aird");
+						
+						IFile oldAirdFile = source.getProject()
+								.getFile(oldRepresentationPath);
+						if (oldAirdFile.exists()) {
+							try {
+								oldAirdFile.delete(false, new NullProgressMonitor());
+								
+								IFile newFile = source.getProject().getFile(newPath);
+								CreateRepresentationAndViewpointHandler.createAll(newFile, newFile.getFileExtension());				
+
+							} catch (CoreException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+						return Status.OK_STATUS;
+					}
+				}.schedule();
+
+	}
+
+	public static void copyAirdFile(IFile source) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void deleteAirdFile(IFile source) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
