6ffd061be6a20be683032384a750ca9d9fabe30a
Rene Trefft <trefftre@studi.informatik.uni-stuttgart.de>
2014-09-29 09:05:51 +0200

    added gitignores for model lqn and fault tree

--- de.uni_stuttgart.iste.cowolf.model.fault_tree/xtend-gen/de/uni_stuttgart/iste/cowolf/model/fault_tree/analyze/XFTAGenerator.java
+++
@@ -1,9 +0,0 @@
-package de.uni_stuttgart.iste.cowolf.model.fault_tree.analyze;
-
-import de.uni_stuttgart.iste.cowolf.model.fault_tree.FaultTree;
-import java.util.Map;
-
-@SuppressWarnings("all")
-public interface XFTAGenerator {
-  public abstract CharSequence doGenerateXFTAFile(final FaultTree root, final Map<String, Object> parameters);
-}
