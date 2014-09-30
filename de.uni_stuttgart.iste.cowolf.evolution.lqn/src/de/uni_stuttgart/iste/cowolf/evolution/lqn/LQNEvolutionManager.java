a17ee4b255489fe2f753467cb6ee37a6236c487d
Philipp Niethammer <webmaster@nochwer.de>
2014-09-30 22:38:58 +0200

    HOTFIX: Disabled LQN coevolution since it throws errors.

--- de.uni_stuttgart.iste.cowolf.evolution.lqn/src/de/uni_stuttgart/iste/cowolf/evolution/lqn/LQNEvolutionManager.java
+++ de.uni_stuttgart.iste.cowolf.evolution.lqn/src/de/uni_stuttgart/iste/cowolf/evolution/lqn/LQNEvolutionManager.java
@@ -1,10 +1,29 @@
 package de.uni_stuttgart.iste.cowolf.evolution.lqn;
 
+import org.eclipse.emf.ecore.resource.Resource;
+
 import de.uni_stuttgart.iste.cowolf.evolution.AbstractEvolutionManager;
 import de.uni_stuttgart.iste.cowolf.evolution.EvolutionTypeInfo;
 import de.uni_stuttgart.iste.cowolf.model.LqnCore.LQN;
 
 public class LQNEvolutionManager extends AbstractEvolutionManager {
+	
+	/**
+	 * FIXME: evolution is not working, disabled.
+	 */
+	@Override
+	public boolean isManaged(Class<?> modelClass) {
+		return false;
+	}
+	
+	/**
+	 * FIXME: evolution is not working, disabled.
+	 */
+	@Override
+	public boolean isManaged(Resource model) {
+		return false;
+	}
+	
 	@Override
 	public EvolutionTypeInfo getEvolutionTypeInfo() {
 		EvolutionTypeInfo info = new EvolutionTypeInfo();
